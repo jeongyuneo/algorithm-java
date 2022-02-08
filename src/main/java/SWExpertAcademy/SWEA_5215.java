b2d5cfc23da4d1a23770ac147ce8a3f23d89fe83
jeonyuneo <piq2255@gmail.com>
2022-02-08 13:31:48 +0900

    [UPDATE] 햄버거 다이어트 - 다른 풀이

--- src/main/java/SWExpertAcademy/SWEA_5215.java
+++ src/main/java/SWExpertAcademy/SWEA_5215.java
@@ -7,7 +7,6 @@ public class SWEA_5215 {
     private static int N;
     private static int limitCalorie;
     private static int[][] ingredients;
-    private static boolean[] isSelected;
     private static int maxScore;
 
     public static void main(String[] args) {
@@ -22,35 +21,27 @@ public class SWEA_5215 {
                     ingredients[i][j] = scanner.nextInt();
                 }
             }
-            isSelected = new boolean[N];
-            subSet(0);
+            subSet(0, 0, 0);
             System.out.printf("#%d %d\n", n, maxScore);
         }
     }
 
-    private static void subSet(int cnt) {
+    private static void subSet(int cnt, int score, int calorie) {
         // 기저 조건
         // step 01. N-1 인덱스까지의 모든 요소 처리 완료 후, 햄버거 재료의 조합이 완성됨
         if (cnt == N) {
-            // step 02. 햄버거의 선호도와 총 칼로리 구하기
-            int kcal = 0;
-            int score = 0;
-            for (int i = 0; i < N; i++) {
-                if (isSelected[i]) {
-                    score += ingredients[i][0];
-                    kcal += ingredients[i][1];
-                }
-            }
-
             // step 03. 총칼로리가 L이하이고 햄버거 선호도가 최댓값이라면 갱신
-            if (kcal <= limitCalorie) {
+            if (calorie <= limitCalorie) {
                 maxScore = Math.max(score, maxScore);
             }
             return;
+        } else if (calorie > limitCalorie) {    // 가지 치기
+            return;
         }
-        isSelected[cnt] = true;
-        subSet(cnt+1);
-        isSelected[cnt] = false;
-        subSet(cnt+1);
+
+        // cnt의 인덱스의 요소를 선택
+        subSet(cnt+1, score+ingredients[cnt][0], calorie+ingredients[cnt][1]);
+        // cnt의 인덱스의 요소를 비선택
+        subSet(cnt+1, score, calorie);
     }
 }
