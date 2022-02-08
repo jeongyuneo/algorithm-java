fc0ad79f9745025f749153630c07cc188f7f1805
jeonyuneo <piq2255@gmail.com>
2022-02-08 21:42:55 +0900

    [UPDATE] 한빈이와 Spot Mart - 재귀 사용ver.

--- src/main/java/SWExpertAcademy/SWEA_9229.java
+++ src/main/java/SWExpertAcademy/SWEA_9229.java
@@ -3,15 +3,14 @@ package SWExpertAcademy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class SWEA_9229 {
 
     private static final BufferedReader BUFFEREDREADER = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer stringTokenizer;
-    private static Stack<Integer> stack;
     private static int[] snacks;
+    private static int result;
     private static int snackNum;
     private static int limit;
 
@@ -28,27 +27,22 @@ public class SWEA_9229 {
             for (int i = 0; i < snackNum; i++) {
                 snacks[i] = Integer.parseInt(stringTokenizer.nextToken());
             }
+            result = -1;
+            find(0, 0, 0);
+            System.out.printf("#%d %d\n", t, result);
+        }
+    }
 
-            int totalWeight = -1;
-            stack = new Stack<>();
-            all: for (int i = 0; i < snackNum-1; i++) {
-                for (int j = i+1; j < snackNum; j++) {
-                    int sum = snacks[i] + snacks[j];
-                    if (sum == limit) {
-                        totalWeight = sum;
-                        break all;
-                    }
-                    if (sum <= limit) {
-                        if (!stack.isEmpty()) {
-                            totalWeight = Math.max(stack.pop(), sum);
-                        } else {
-                            totalWeight = sum;
-                        }
-                        stack.push(totalWeight);
-                    }
-                }
-            }
-            System.out.printf("#%d %d\n", t, totalWeight);
+    private static void find(int cnt, int start, int sum) {
+        if (sum > limit) {
+            return;
+        }
+        if (cnt == 2) {
+            result = Math.max(sum, result);
+            return;
+        }
+        for (int i = start; i < snackNum; i++) {
+            find(cnt+1, i+1, sum+snacks[i]);
         }
     }
 }
