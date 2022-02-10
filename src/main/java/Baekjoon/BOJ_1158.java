e73038071146783754e29a5cdee1b7c22c6959ff
jeonyuneo <piq2255@gmail.com>
2022-02-10 13:55:50 +0900

    [ADD] 요세푸스 문제

---
+++ src/main/java/Baekjoon/BOJ_1158.java
@@ -0,0 +1,42 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_1158 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int k = Integer.parseInt(stringTokenizer.nextToken());
+
+        List<Integer> array = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            array.add(i);
+        }
+        int idx = k-1;
+        stringBuilder.append("<");
+        while (array.size() > 0) {
+            stringBuilder.append(array.get(idx))
+                    .append(", ");
+            array.remove(idx--);
+            int cnt = 0;
+            while (cnt < k) {
+                cnt++;
+                if (++idx >= array.size()) {
+                    idx = 0;
+                }
+            }
+        }
+        stringBuilder.setLength(stringBuilder.length()-2);
+        stringBuilder.append(">");
+        System.out.println(stringBuilder);
+    }
+}
