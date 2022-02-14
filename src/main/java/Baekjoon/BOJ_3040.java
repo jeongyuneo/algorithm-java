3b593b62c0b17a4774d1ffeb3a3afb74bbdb0523
jeonyuneo <piq2255@gmail.com>
2022-02-14 23:45:02 +0900

    [ADD] 백설 공주와 일곱 난쟁이

---
+++ src/main/java/Baekjoon/BOJ_3040.java
@@ -0,0 +1,38 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BOJ_3040 {
+
+    private static List<Integer> list = new ArrayList<>();
+    private static int[] heights;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        heights = new int[9];
+        for (int i = 0; i < 9; i++) {
+            heights[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+        all: for (int i = 0; i < 8; i++) {
+            for (int j = i+1; j < 9; j++) {
+                int sum = 0;
+                for (int k = 0; k < 9; k++) {
+                    if (k != i && k != j) {
+                        sum += heights[k];
+                        list.add(heights[k]);
+                    }
+                }
+                if (sum == 100) {
+                    break all;
+                } else {
+                    list.clear();
+                }
+            }
+        }
+        list.forEach(System.out::println);
+    }
+}
