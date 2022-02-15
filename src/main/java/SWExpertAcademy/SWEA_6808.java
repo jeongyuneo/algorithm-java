87e196c1507e9ce57e4ef98990954a61ed1b5541
jeonyuneo <piq2255@gmail.com>
2022-02-15 13:09:50 +0900

    [UPDATE] 규영이와 인영이의 카드게임 - 순열 생성 NextPermutation  적용

--- src/main/java/SWExpertAcademy/SWEA_6808.java
+++ src/main/java/SWExpertAcademy/SWEA_6808.java
@@ -3,16 +3,14 @@ package SWExpertAcademy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class SWEA_6808 {
 
-    private static final int CARD_NUMBER = 18;
-    private static final List<Integer> GYUYEONG_CARDS = new ArrayList<>();
-    private static final List<Integer> INYEONG_CARDS = new ArrayList<>();
-    private static final int[] SELECTED_INYEONG_CARDS = new int[9];
+    private static final int TOTAL_CARD_NUMBER = 18;
+    private static final int PLAYER_CARD_NUMBER = 9;
+    private static final int[] GYUYEONG_CARDS = new int[PLAYER_CARD_NUMBER];
+    private static final int[] INYEONG_CARDS = new int[PLAYER_CARD_NUMBER];
 
     private static int gyuyeongWin;
     private static int gyuyeongLose;
@@ -26,26 +24,29 @@ public class SWEA_6808 {
                     .append(t)
                     .append(" ");
 
-            boolean[] isSelected = new boolean[CARD_NUMBER+1];
+            boolean[] isSelected = new boolean[TOTAL_CARD_NUMBER +1];
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            for (int i = 0; i < 9; i++) {
+            for (int i = 0; i < PLAYER_CARD_NUMBER; i++) {
                 isSelected[Integer.parseInt(stringTokenizer.nextToken())] = true;
             }
-            for (int i = 1; i <= CARD_NUMBER; i++) {
+            int gyuyeongIndex = 0;
+            int inyeongIndex = 0;
+            for (int i = 1; i <= TOTAL_CARD_NUMBER; i++) {
                 if (!isSelected[i]) {
-                    INYEONG_CARDS.add(i);
+                    INYEONG_CARDS[inyeongIndex++] = i;
                 } else {
-                    GYUYEONG_CARDS.add(i);
+                    GYUYEONG_CARDS[gyuyeongIndex++] = i;
                 }
             }
 
             gyuyeongWin = 0;
             gyuyeongLose = 0;
 
-            permutation(0, 0);
+            Arrays.sort(INYEONG_CARDS);
 
-            GYUYEONG_CARDS.clear();
-            INYEONG_CARDS.clear();
+            do {
+                play();
+            } while (nextPermutation());
 
             stringBuilder.append(gyuyeongWin)
                     .append(" ")
@@ -55,32 +56,53 @@ public class SWEA_6808 {
         System.out.println(stringBuilder);
     }
 
-    private static void permutation(int cnt, int flag) {
-        if (cnt == 9) {
-            int gyuyeongScore = 0;
-            int inyeongScore = 0;
-            for (int j = 0; j < 9; j++) {
-                int gyuyeongCard = GYUYEONG_CARDS.get(j);
-                int inyeongCard = SELECTED_INYEONG_CARDS[j];
-                if (gyuyeongCard > inyeongCard) {
-                    gyuyeongScore += gyuyeongCard + inyeongCard;
-                } else if ((gyuyeongCard < inyeongCard)) {
-                    inyeongScore += gyuyeongCard + inyeongCard;
-                }
+    private static void play() {
+        int gyuyeongScore = 0;
+        int inyeongScore = 0;
+        for (int j = 0; j < PLAYER_CARD_NUMBER; j++) {
+            int gyuyeongCard = GYUYEONG_CARDS[j];
+            int inyeongCard = INYEONG_CARDS[j];
+            if (gyuyeongCard > inyeongCard) {
+                gyuyeongScore += gyuyeongCard + inyeongCard;
+            } else if ((gyuyeongCard < inyeongCard)) {
+                inyeongScore += gyuyeongCard + inyeongCard;
             }
-            if (gyuyeongScore > inyeongScore) {
-                gyuyeongWin++;
-            } else if (gyuyeongScore < inyeongScore) {
-                gyuyeongLose++;
-            }
-            return;
         }
-        for (int i = 0; i < 9; i++) {
-            if ((flag & 1 << i) != 0) {
-                continue;
-            }
-            SELECTED_INYEONG_CARDS[cnt] = INYEONG_CARDS.get(i);
-            permutation(cnt+1, flag | 1 << i);
+        if (gyuyeongScore > inyeongScore) {
+            gyuyeongWin++;
+        } else if (gyuyeongScore < inyeongScore) {
+            gyuyeongLose++;
         }
+    }
+
+    private static boolean nextPermutation() {
+        int i = PLAYER_CARD_NUMBER - 1;
+        while (i > 0 && INYEONG_CARDS[i-1] >= INYEONG_CARDS[i]) {
+            i--;
+        }
+
+        if (i == 0) {
+            return false;
+        }
+
+        int j = PLAYER_CARD_NUMBER - 1;
+        while (INYEONG_CARDS[i-1] >= INYEONG_CARDS[j]) {
+            j--;
+        }
+
+        swqp(i-1, j);
+
+        int k = PLAYER_CARD_NUMBER - 1;
+        while (i < k) {
+            swqp(i++, k--);
+        }
+
+        return true;
+    }
+
+    private static void swqp(int i, int j) {
+        int temp = INYEONG_CARDS[i];
+        INYEONG_CARDS[i] = INYEONG_CARDS[j];
+        INYEONG_CARDS[j] = temp;
     }
 }
