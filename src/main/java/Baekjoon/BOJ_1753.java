c05cb80cc90e9cebe0f28a628732fa8c04f29438
jeonyuneo <piq2255@gmail.com>
2022-02-24 16:53:21 +0900

    [UPDATE] 최단경로 - 최소 힙 적용

--- src/main/java/Baekjoon/BOJ_1753.java
+++ src/main/java/Baekjoon/BOJ_1753.java
@@ -3,12 +3,25 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_1753 {
+
+    static class Node implements Comparable<Node>{
+        
+        int vertex;
+        int weight;
+
+        public Node(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +30,7 @@ public class BOJ_1753 {
         int v = Integer.parseInt(stringTokenizer.nextToken());
         int e = Integer.parseInt(stringTokenizer.nextToken());
         int k = Integer.parseInt(bufferedReader.readLine());
-        List<int[]>[] adjList = new List[v+1];
+        List<Node>[] adjList = new List[v+1];
         for (int i = 1; i <= v; i++) {
             adjList[i] = new ArrayList<>();
         }
@@ -26,32 +39,28 @@ public class BOJ_1753 {
             int from = Integer.parseInt(stringTokenizer.nextToken());
             int to = Integer.parseInt(stringTokenizer.nextToken());
             int w = Integer.parseInt(stringTokenizer.nextToken());
-            adjList[from].add(new int[]{to, w});
+            adjList[from].add(new Node(to, w));
         }
 
         int[] distance = new int[v+1];
         boolean[] visited = new boolean[v+1];
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        priorityQueue.offer(new Node(k, 0));
 
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[k] = 0;
-        for (int i = 1; i <= v; i++) {
-            int min = Integer.MAX_VALUE;
-            int current = 0;
-            for (int j = 1; j <= v; j++) {
-                if (!visited[j] && min > distance[j]) {
-                    min = distance[j];
-                    current = j;
-                }
+        while (!priorityQueue.isEmpty()) {
+            Node current = priorityQueue.poll();
+            if (visited[current.vertex]) {
+                continue;
             }
-            visited[current] = true;
-            if (current == 0) {
-                break;
-            }
+            visited[current.vertex] = true;
 
-            for (int j = 0; j < adjList[current].size(); j++) {
-                int[] node = adjList[current].get(j);
-                if (!visited[node[0]] && distance[node[0]] > distance[current] + node[1]) {
-                    distance[node[0]] = distance[current] + node[1];
+            for (int j = 0; j < adjList[current.vertex].size(); j++) {
+                Node node = adjList[current.vertex].get(j);
+                if (distance[node.vertex] > distance[current.vertex] + node.weight) {
+                    distance[node.vertex] = distance[current.vertex] + node.weight;
+                    priorityQueue.offer(new Node(node.vertex, distance[node.vertex]));
                 }
             }
         }
