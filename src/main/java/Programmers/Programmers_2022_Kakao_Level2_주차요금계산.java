38b53ecae82eeac412d5eb6b5014ed3830322784
jeonyuneo <piq2255@gmail.com>
2022-05-17 22:53:14 +0900

    [UPDATE] 2022-카카오-주차 요금 계산 - 파일명 수정

--- src/main/java/Programmers/Programmers_2022_Kakao_Level2_주차요금계산.java
+++ src/main/java/Programmers/Programmers_2022_Kakao_Level2_주차요금계산.java
@@ -2,7 +2,7 @@ package Programmers;
 
 import java.util.*;
 
-public class Programmers_2022_Kakao_주차요금계산 {
+public class Programmers_2022_Kakao_Level2_주차요금계산 {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solution(new int[]{180, 5000, 10, 600}, new String[]{"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"})));
