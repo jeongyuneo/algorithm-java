c041c88a05fb718ef3bf36a2011d0a1e1f703c02
jeonyuneo <piq2255@gmail.com>
2022-05-17 22:52:58 +0900

    [UPDATE] 2020-카카오-문자열 압축 - 파일명 수정

--- src/main/java/Programmers/Programmers_2020_Kakao_Level2_문자열압축.java
+++ src/main/java/Programmers/Programmers_2020_Kakao_Level2_문자열압축.java
@@ -1,6 +1,6 @@
 package Programmers;
 
-public class Programmers_2020_Kakao_문자열압축 {
+public class Programmers_2020_Kakao_Level2_문자열압축 {
 
     public static void main(String[] args) {
         System.out.println(solution("aabbaccc"));
