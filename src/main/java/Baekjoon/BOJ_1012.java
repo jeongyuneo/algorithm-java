5dde4e76ad92498e9573d424cc39bc16329ec40d
jeonyuneo <piq2255@gmail.com>
2022-08-20 15:03:38 +0900

    [UPDATE] 유기농 배추 - 재풀이

--- src/main/java/Baekjoon/BOJ_1012.java
+++ src/main/java/Baekjoon/BOJ_1012.java
@@ -9,62 +9,58 @@ import java.util.StringTokenizer;
 
 public class BOJ_1012 {
 
-    private static final int[][] DELTAS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    private static final int CROP = 1;
-    private static final int VISIT = 2;
-
-    private static int n;
-    private static int m;
-    private static int worm;
+    private static final int[][] DELTAS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private static final Queue<int[]> CHINESE_CABBAGES = new LinkedList<>();
+    private static final int CHINESE_CABBAGE = 1;
+    private static final int EAT = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = null;
-        StringBuilder stringBuilder = new StringBuilder();
         int testCase = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder answer = new StringBuilder();
         for (int t = 0; t < testCase; t++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            m = Integer.parseInt(stringTokenizer.nextToken());
-            n = Integer.parseInt(stringTokenizer.nextToken());
-            int cropNum = Integer.parseInt(stringTokenizer.nextToken());
-            int[][] field = new int[n][m];
-            for (int i = 0; i < cropNum; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int width = Integer.parseInt(stringTokenizer.nextToken());
+            int height = Integer.parseInt(stringTokenizer.nextToken());
+            int[][] farm = new int[height][width];
+            int chineseCabbageCount = Integer.parseInt(stringTokenizer.nextToken());
+            for (int i = 0; i < chineseCabbageCount; i++) {
                 stringTokenizer = new StringTokenizer(bufferedReader.readLine());
                 int y = Integer.parseInt(stringTokenizer.nextToken());
                 int x = Integer.parseInt(stringTokenizer.nextToken());
-                field[x][y] = 1;
+                farm[x][y] = CHINESE_CABBAGE;
             }
-            worm = 0;
-            countWorms(field);
-            stringBuilder.append(worm)
+
+            answer.append(getInsectCount(width, height, farm))
                     .append("\n");
         }
-        System.out.println(stringBuilder);
+        System.out.println(answer);
     }
 
-    private static void countWorms(int[][] field) {
-        Queue<int[]> crops = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (field[i][j] == CROP) {
-                    worm++;
-                    crops.offer(new int[]{i, j});
-                    while (!crops.isEmpty()) {
-                        int[] crop = crops.poll();
-                        int x = crop[0];
-                        int y = crop[1];
-                        field[x][y] = VISIT;
+    private static int getInsectCount(int width, int height, int[][] farm) {
+        int insectCount = 0;
+        for (int x = 0; x < height; x++) {
+            for (int y = 0; y < width; y++) {
+                if (farm[x][y] == CHINESE_CABBAGE) {
+                    insectCount++;
+                    CHINESE_CABBAGES.offer(new int[]{x, y});
+                    while (!CHINESE_CABBAGES.isEmpty()) {
+                        int[] current = CHINESE_CABBAGES.poll();
+                        int currentX = current[0];
+                        int currentY = current[1];
+                        farm[currentX][currentY] = EAT;
                         for (int[] delta : DELTAS) {
-                            int dx = x + delta[0];
-                            int dy = y + delta[1];
-                            if (dx >= 0 && dx < n && dy >= 0 && dy < m && field[dx][dy] == CROP) {
-                                field[dx][dy] = VISIT;
-                                crops.offer(new int[]{dx, dy});
+                            int dx = currentX + delta[0];
+                            int dy = currentY + delta[1];
+                            if (dx >= 0 && dx < height && dy >= 0 && dy < width && farm[dx][dy] == CHINESE_CABBAGE) {
+                                farm[dx][dy] = EAT;
+                                CHINESE_CABBAGES.offer(new int[]{dx, dy});
                             }
                         }
                     }
                 }
             }
         }
+        return insectCount;
     }
 }
