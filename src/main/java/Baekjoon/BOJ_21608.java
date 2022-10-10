af1fc7d01e7582c7d57f1c10f5f793ba450fdf1e
jeonyuneo <piq2255@gmail.com>
2022-10-11 01:58:19 +0900

    [UPDATE] 상어 초등학교 - 재풀이

--- src/main/java/Baekjoon/BOJ_21608.java
+++ src/main/java/Baekjoon/BOJ_21608.java
@@ -9,87 +9,81 @@ import java.util.StringTokenizer;
 
 public class BOJ_21608 {
 
-    private static final int[][] DELTAS = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    private static final int[][] DELTAS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private static final int EMPTY = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
         int[][] classroom = new int[n][n];
-        int studentNum = (int) Math.pow(n, 2);
-        List[] studentInfos = new List[studentNum + 1];
-        for (int i = 0, studentInfosLength = studentInfos.length; i < studentInfosLength; i++) {
-            studentInfos[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < studentNum; i++) {
+        List[] favorites = new List[n * n + 1];
+        for (int i = 0; i < n * n; i++) {
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int student = Integer.parseInt(stringTokenizer.nextToken());
-            int first = Integer.parseInt(stringTokenizer.nextToken());
-            int second = Integer.parseInt(stringTokenizer.nextToken());
-            int third = Integer.parseInt(stringTokenizer.nextToken());
-            int fourth = Integer.parseInt(stringTokenizer.nextToken());
+            favorites[student] = new ArrayList<>();
+            favorites[student].add(Integer.parseInt(stringTokenizer.nextToken()));
+            favorites[student].add(Integer.parseInt(stringTokenizer.nextToken()));
+            favorites[student].add(Integer.parseInt(stringTokenizer.nextToken()));
+            favorites[student].add(Integer.parseInt(stringTokenizer.nextToken()));
 
-            studentInfos[student].add(first);
-            studentInfos[student].add(second);
-            studentInfos[student].add(third);
-            studentInfos[student].add(fourth);
-
-            int[] location = new int[4];
-            location[0] = n;
-            location[1] = n;
+            int preFavorite = 0;
+            int preEmpty = 0;
+            int preX = n;
+            int preY = n;
             for (int x = 0; x < n; x++) {
                 for (int y = 0; y < n; y++) {
-                    if (classroom[x][y] == 0) {
+                    if (classroom[x][y] == EMPTY) {
                         int favorite = 0;
                         int empty = 0;
                         for (int[] delta : DELTAS) {
                             int dx = x + delta[0];
                             int dy = y + delta[1];
                             if (dx >= 0 && dx < n && dy >= 0 && dy < n) {
-                                if (studentInfos[student].contains(classroom[dx][dy])) {
-                                    favorite++;
-                                } else if (classroom[dx][dy] == 0) {
-                                    empty++;
+                                if (classroom[dx][dy] == EMPTY) {
+                                    ++empty;
+                                } else if (favorites[student].contains(classroom[dx][dy])) {
+                                    ++favorite;
                                 }
                             }
                         }
-                        if (favorite > location[2]) {
-                            location[0] = x;
-                            location[1] = y;
-                            location[2] = favorite;
-                            location[3] = empty;
-                        } else if (favorite == location[2] && empty > location[3]) {
-                            location[0] = x;
-                            location[1] = y;
-                            location[3] = empty;
-                        } else if (favorite == location[2] && empty == location[3] && location[0] > x) {
-                            location[0] = x;
-                            location[1] = y;
-                        } else if (favorite == location[2] && empty == location[3] && location[0] == x && location[1] > y) {
-                            location[1] = y;
+
+                        if (preFavorite < favorite) {
+                            preFavorite = favorite;
+                            preEmpty = empty;
+                            preX = x;
+                            preY = y;
+                        } else if (preFavorite == favorite && preEmpty < empty) {
+                            preEmpty = empty;
+                            preX = x;
+                            preY = y;
+                        } else if (preFavorite == favorite && preEmpty == empty && preX > x) {
+                            preX = x;
+                            preY = y;
+                        } else if (preFavorite == favorite && preEmpty == empty && preX == x && preY > y) {
+                            preY = y;
                         }
                     }
                 }
             }
-            classroom[location[0]][location[1]] = student;
+            classroom[preX][preY] = student;
         }
 
-        int total = 0;
+        int sum = 0;
         for (int x = 0; x < n; x++) {
             for (int y = 0; y < n; y++) {
-                int student = classroom[x][y];
                 int favorite = 0;
                 for (int[] delta : DELTAS) {
                     int dx = x + delta[0];
                     int dy = y + delta[1];
-                    if (dx >= 0 && dx < n && dy >= 0 && dy < n && studentInfos[student].contains(classroom[dx][dy])) {
-                        favorite++;
+                    if (dx >= 0 && dx < n && dy >= 0 && dy < n && favorites[classroom[x][y]].contains(classroom[dx][dy])) {
+                        ++favorite;
                     }
                 }
                 if (favorite != 0) {
-                    total += Math.pow(10, favorite - 1);
+                    sum += Math.pow(10, favorite - 1);
                 }
             }
         }
-        System.out.println(total);
+        System.out.println(sum);
     }
 }
