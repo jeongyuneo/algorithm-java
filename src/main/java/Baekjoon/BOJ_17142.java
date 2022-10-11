03b4551b78451fed23638b0399e791868e4e2557
jeonyuneo <piq2255@gmail.com>
2022-10-11 15:19:08 +0900

    [UPDATE] 연구소 3 - 재풀이

--- src/main/java/Baekjoon/BOJ_17142.java
+++ src/main/java/Baekjoon/BOJ_17142.java
@@ -8,18 +8,17 @@ import java.util.*;
 public class BOJ_17142 {
 
     private static final int[][] DELTAS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    private static final List<int[]> VIRUS = new ArrayList<>();
+    private static final List<int[]> VIRUSES = new ArrayList<>();
     private static final int EMPTY = 0;
     private static final int WALL = 1;
-    private static final int INACTIVE_VIRUS = 2;
-    private static final int ACTIVE_VIRUS = 3;
+    private static final int VIRUS = 2;
 
     private static int[][] laboratory;
+    private static int[][] activeVirus;
     private static int n;
     private static int m;
-    private static int emptyPlace;
+    private static int emptySpace;
     private static int minTime;
-    private static boolean canAllSpread;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -32,83 +31,67 @@ public class BOJ_17142 {
             for (int j = 0; j < n; j++) {
                 int input = Integer.parseInt(stringTokenizer.nextToken());
                 laboratory[i][j] = input;
-                if (input == INACTIVE_VIRUS) {
-                    VIRUS.add(new int[]{i, j});
-                } else if (input == EMPTY) {
-                    emptyPlace++;
+                if (input == EMPTY) {
+                    ++emptySpace;
+                } else if (input == VIRUS) {
+                    VIRUSES.add(new int[]{i, j});
                 }
             }
         }
 
-        minTime = 0;
-        if (emptyPlace != 0) {
-            minTime = n * n;
-            selectVirus(new int[m][2], 0, 0);
-            if (!canAllSpread) {
-                minTime = -1;
-            }
-        }
-        System.out.println(minTime);
+        minTime = Integer.MAX_VALUE;
+        activeVirus = new int[m][2];
+        activateVirus(0, 0);
+        System.out.println(minTime == Integer.MAX_VALUE ? -1 : minTime);
     }
 
-    private static void selectVirus(int[][] selectedVirus, int cnt, int start) {
+    private static void activateVirus(int cnt, int start) {
         if (cnt == m) {
-            spread(selectedVirus);
+            spreadVirus();
             return;
         }
 
-        for (int i = start; i < VIRUS.size(); i++) {
-            selectedVirus[cnt] = VIRUS.get(i);
-            selectVirus(selectedVirus, cnt + 1, i + 1);
+        for (int i = start; i < VIRUSES.size(); i++) {
+            activeVirus[cnt] = VIRUSES.get(i);
+            activateVirus(cnt + 1, i + 1);
         }
     }
 
-    private static void spread(int[][] selectedVirus) {
-        int[][] laboratoryWithVirus = copy();
-        Queue<int[]> queue = new LinkedList<>();
-        for (int[] virus : selectedVirus) {
-            queue.offer(virus);
-        }
+    private static void spreadVirus() {
+        Queue<int[]> queue = new ArrayDeque<>();
         boolean[][] isVisited = new boolean[n][n];
+        for (int[] virus : activeVirus) {
+            queue.offer(virus);
+            isVisited[virus[0]][virus[1]] = true;
+        }
+        int empty = emptySpace;
         int time = 0;
-        int empty = emptyPlace;
         while (!queue.isEmpty()) {
-            time++;
+            if (empty == 0) {
+                break;
+            }
+
             int size = queue.size();
+            ++time;
             while (size-- > 0) {
                 int[] current = queue.poll();
                 int x = current[0];
                 int y = current[1];
-                isVisited[x][y] = true;
                 for (int[] delta : DELTAS) {
                     int dx = x + delta[0];
                     int dy = y + delta[1];
-                    if (dx >= 0 && dx < n && dy >= 0 && dy < n && !isVisited[dx][dy] && laboratoryWithVirus[dx][dy] != WALL) {
-                        isVisited[dx][dy] = true;
-                        if (laboratoryWithVirus[dx][dy] == EMPTY) {
-                            empty--;
-                        }
-                        laboratoryWithVirus[dx][dy] = ACTIVE_VIRUS;
+                    if (dx >= 0 && dx < n && dy >= 0 && dy < n && laboratory[dx][dy] != WALL && !isVisited[dx][dy]) {
                         queue.offer(new int[]{dx, dy});
-                    }
-                    if (empty == 0) {
-                        canAllSpread = true;
-                        minTime = Math.min(minTime, time);
-                        return;
+                        isVisited[dx][dy] = true;
+                        if (laboratory[dx][dy] == EMPTY) {
+                            --empty;
+                        }
                     }
                 }
-
             }
         }
-    }
-
-    private static int[][] copy() {
-        int[][] laboratoryWithVirus = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                laboratoryWithVirus[i][j] = laboratory[i][j];
-            }
+        if (empty == 0) {
+            minTime = Math.min(minTime, time);
         }
-        return laboratoryWithVirus;
     }
 }
