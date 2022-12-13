b38f32c48940b72a8aa6af3a56c47ecbecdce992
jeonyuneo <piq2255@gmail.com>
2022-12-13 19:05:18 +0900

    [UPDATE] 문제 추천 시스템 Version 1 - 문제 난이도 저장 자료구조 개선

--- src/main/java/Baekjoon/BOJ_21939.java
+++ src/main/java/Baekjoon/BOJ_21939.java
@@ -26,6 +26,7 @@ public class BOJ_21939 {
         }
     }
 
+    private static final int[] PROBLEMS = new int[100001];
     private static final String RECOMMEND = "recommend";
     private static final String ADD = "add";
     private static final String SOLVED = "solved";
@@ -37,13 +38,12 @@ public class BOJ_21939 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
         TreeSet<Problem> problems = new TreeSet<>();
-        Map<Integer, Integer> problemInfo = new HashMap<>();
         for (int i = 0; i < n; i++) {
             String[] inputs = bufferedReader.readLine().split(DELIMITER);
             int number = Integer.parseInt(inputs[PROBLEM]);
             int level = Integer.parseInt(inputs[LEVEL]);
             problems.add(new Problem(number, level));
-            problemInfo.put(number, level);
+            PROBLEMS[number] = level;
         }
 
         StringBuilder answer = new StringBuilder();
@@ -64,12 +64,11 @@ public class BOJ_21939 {
                 number = Integer.parseInt(stringTokenizer.nextToken());
                 int level = Integer.parseInt(stringTokenizer.nextToken());
                 problems.add(new Problem(number, level));
-                problemInfo.put(number, level);
+                PROBLEMS[number] = level;
             } else if (operator.equals(SOLVED)) {
                 number = Integer.parseInt(stringTokenizer.nextToken());
-                int level = problemInfo.get(number);
+                int level = PROBLEMS[number];
                 problems.remove(new Problem(number, level));
-                problemInfo.remove(number);
             }
         }
         System.out.println(answer);
