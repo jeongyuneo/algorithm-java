aa41b3ba18bd2e1558c09a1cfb81fb6bbe79bd4c
jeonyuneo <piq2255@gmail.com>
2022-12-14 22:56:10 +0900

    [UPDATE] 최소비용 구하기 - 개선

--- src/main/java/Baekjoon/BOJ_1916.java
+++ src/main/java/Baekjoon/BOJ_1916.java
@@ -7,15 +7,14 @@ import java.util.*;
 
 public class BOJ_1916 {
 
-    private static final int STARTING_CITY = 0;
-    private static final int ARRIVING_CITY = 1;
-    private static final int COST = 2;
+    private static final int CITY = 0;
+    private static final int COST = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
         int m = Integer.parseInt(bufferedReader.readLine());
-        List<int[]>[] busInfos = new List[n+1];
+        List<int[]>[] busInfos = new List[n + 1];
         for (int i = 1; i <= n; i++) {
             busInfos[i] = new ArrayList<>();
         }
@@ -24,32 +23,31 @@ public class BOJ_1916 {
             int from = Integer.parseInt(stringTokenizer.nextToken());
             int to = Integer.parseInt(stringTokenizer.nextToken());
             int cost = Integer.parseInt(stringTokenizer.nextToken());
-            busInfos[from].add(new int[]{from, to, cost});
+            busInfos[from].add(new int[]{to, cost});
         }
-
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int start = Integer.parseInt(stringTokenizer.nextToken());
         int arrive = Integer.parseInt(stringTokenizer.nextToken());
+
         int[] costs = new int[n + 1];
         Arrays.fill(costs, Integer.MAX_VALUE);
         costs[start] = 0;
         PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparing(busInfo -> busInfo[COST]));
-        priorityQueue.addAll(busInfos[start]);
+        priorityQueue.offer(new int[]{start, 0});
         while (!priorityQueue.isEmpty()) {
             int[] current = priorityQueue.poll();
-            int from = current[STARTING_CITY];
-            int to = current[ARRIVING_CITY];
-            int cost = current[COST];
+            int currentCity = current[CITY];
 
-            if (costs[to] == 0) {
-                continue;
+            if (currentCity == arrive) {
+                break;
             }
 
-            int nextCost = costs[from] + cost;
-            if (costs[to] > nextCost) {
-                costs[to] = nextCost;
-                for (int[] city : busInfos[to]) {
-                    priorityQueue.offer(city);
+            for (int[] next : busInfos[currentCity]) {
+                int nextCity = next[CITY];
+                int nextCost = next[COST] + current[COST];
+                if (costs[nextCity] > nextCost) {
+                    costs[nextCity] = nextCost;
+                    priorityQueue.offer(new int[]{nextCity, nextCost});
                 }
             }
         }
