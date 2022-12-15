bd0b5d686dd5cf6a1e464ae75b1c86cac5448cef
jeonyuneo <piq2255@gmail.com>
2022-12-15 16:39:01 +0900

    [ADD] 전화번호 목록

---
+++ src/main/java/Programmers/Programmers_해시_Level2_전화번호목록.java
@@ -0,0 +1,22 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Programmers_해시_Level2_전화번호목록 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"119", "97674223", "1195524421"}));
+        System.out.println(solution(new String[]{"123", "456", "789"}));
+        System.out.println(solution(new String[]{"12", "123", "1235", "567", "88"}));
+    }
+
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
