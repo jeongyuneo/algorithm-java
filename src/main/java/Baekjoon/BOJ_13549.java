900e47507c128c8dc29a313859ea5521c47f494f
jeonyuneo <piq2255@gmail.com>
2022-12-15 15:29:24 +0900

    [UPDATE] 숨바꼭질 3 - 자료구조 개선

--- src/main/java/Baekjoon/BOJ_13549.java
+++ src/main/java/Baekjoon/BOJ_13549.java
@@ -18,10 +18,10 @@ public class BOJ_13549 {
         int[] space = new int[100001];
         Arrays.fill(space, Integer.MAX_VALUE);
         space[n] = 0;
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{n, 0});
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.offer(new int[]{n, 0});
+        while (!deque.isEmpty()) {
+            int[] current = deque.poll();
             int location = current[LOCATION];
             if (location == k) {
                 break;
@@ -30,12 +30,12 @@ public class BOJ_13549 {
             for (int move = -1; move <= 1; move += 2) {
                 if (location + move >= 0 && location + move <= 100000 && space[location + move] > time + 1) {
                     space[location + move] = time + 1;
-                    queue.offer(new int[]{location + move, time + 1});
+                    deque.offer(new int[]{location + move, time + 1});
                 }
             }
             if (location * 2 <= 100000 && space[location * 2] > time) {
                 space[location * 2] = time;
-                queue.offer(new int[]{location * 2, time});
+                deque.offerFirst(new int[]{location * 2, time});
             }
         }
         System.out.println(space[k]);
