005942ef4dccb4409069685794e1073db1d06063
jeonyuneo <piq2255@gmail.com>
2022-12-22 17:44:37 +0900

    [UPDATE] 위장 - DP 사용

--- src/main/java/Programmers/Programmers_해시_Level2_위장.java
+++ src/main/java/Programmers/Programmers_해시_Level2_위장.java
@@ -8,6 +8,8 @@ public class Programmers_해시_Level2_위장 {
 
     private static final int NAME = 0;
     private static final int TYPE = 1;
+    private static final int DO_NOT_PUT_ON = 0;
+    private static final int PUT_ON = 1;
 
     public static void main(String[] args) {
         System.out.println(solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
@@ -15,11 +17,21 @@ public class Programmers_해시_Level2_위장 {
     }
 
     public static int solution(String[][] clothes) {
-        return Arrays.stream(clothes)
+        int[] counts = Arrays.stream(clothes)
                 .collect(groupingBy(clothe -> clothe[TYPE], mapping(clothe -> clothe[NAME], counting())))
                 .values()
                 .stream()
-                .reduce(1L, (a, b) -> a * (b + 1))
-                .intValue() - 1;
+                .mapToInt(Long::intValue)
+                .toArray();
+        int kind = counts.length;
+        int[][] cases = new int[kind][2];
+        cases[0][DO_NOT_PUT_ON] = 1;
+        cases[0][PUT_ON] = counts[0];
+        for (int i = 1; i < kind; i++) {
+            int previous = i - 1;
+            cases[i][DO_NOT_PUT_ON] = cases[previous][DO_NOT_PUT_ON] + cases[previous][PUT_ON];
+            cases[i][PUT_ON] = cases[previous][DO_NOT_PUT_ON] * counts[i] + cases[previous][PUT_ON] * counts[i];
+        }
+        return cases[kind - 1][DO_NOT_PUT_ON] + cases[kind - 1][PUT_ON] - 1;
     }
 }
