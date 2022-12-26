1bf3ddd7ef83d01ba99bebb72bf9f68f42331ffb
jeonyuneo <piq2255@gmail.com>
2022-12-26 18:18:56 +0900

    [UPDATE] 문자열 게임 2 - 개선

--- src/main/java/Baekjoon/BOJ_20437.java
+++ src/main/java/Baekjoon/BOJ_20437.java
@@ -3,6 +3,10 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BOJ_20437 {
 
@@ -11,7 +15,7 @@ public class BOJ_20437 {
         int t = Integer.parseInt(bufferedReader.readLine());
         StringBuilder answer = new StringBuilder();
         for (int i = 0; i < t; i++) {
-            String w = bufferedReader.readLine();
+            char[] w = bufferedReader.readLine().toCharArray();
             int k = Integer.parseInt(bufferedReader.readLine());
 
             if (k == 1) {
@@ -19,19 +23,22 @@ public class BOJ_20437 {
                 continue;
             }
 
-            int minLength = w.length() + 1;
+            Map<Character, List<Integer>> indexInfo = new HashMap<>();
+            int[] counts = new int[26];
+            for (int j = 0, wLength = w.length; j < wLength; j++) {
+                char c = w[j];
+                indexInfo.computeIfAbsent(c, index -> new ArrayList<>()).add(j);
+                counts[c - 97]++;
+            }
+
+            int minLength = w.length;
             int maxLength = -1;
-            for (int j = 0; j < w.length() - k; j++) {
-                char character = w.charAt(j);
-                int count = 1;
-                for (int l = j + 1; l < w.length(); l++) {
-                    if (character == w.charAt(l)) {
-                        count++;
-                    }
-                    if (count == k) {
-                        minLength = Math.min(minLength, l - j + 1);
-                        maxLength = Math.max(maxLength, l - j + 1);
-                        break;
+            for (Map.Entry<Character, List<Integer>> indexes : indexInfo.entrySet()) {
+                if (counts[indexes.getKey() - 97] >= k) {
+                    List<Integer> index = indexes.getValue();
+                    for (int j = 0; j <= index.size() - k; j++) {
+                        minLength = Math.min(minLength, index.get(j + k - 1) - index.get(j) + 1);
+                        maxLength = Math.max(maxLength, index.get(j + k - 1) - index.get(j) + 1);
                     }
                 }
             }
