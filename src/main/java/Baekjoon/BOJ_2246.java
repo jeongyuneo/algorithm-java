085f0c2d675cf47d32da1b8263dab8673d055c67
jeonyuneo <piq2255@gmail.com>
2022-12-26 20:18:30 +0900

    [UPDATE] 콘도 선정 - 개선

--- src/main/java/Baekjoon/BOJ_2246.java
+++ src/main/java/Baekjoon/BOJ_2246.java
@@ -7,54 +7,32 @@ import java.util.*;
 
 public class BOJ_2246 {
 
-    private static final int NUMBER = 0;
-    private static final int DISTANCE = 1;
-    private static final int COST = 2;
+    private static final int DISTANCE = 0;
+    private static final int COST = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
-        int[][] condos = new int[n][3];
+        int[][] condos = new int[n][2];
         for (int i = 0; i < n; i++) {
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            condos[i][NUMBER] = i;
             condos[i][DISTANCE] = Integer.parseInt(stringTokenizer.nextToken());
             condos[i][COST] = Integer.parseInt(stringTokenizer.nextToken());
         }
-        List<Integer> candidatesForCase1 = getCandidates(n, condos, DISTANCE, COST);
-        List<Integer> candidatesForCase2 = getCandidates(n, condos, COST, DISTANCE);
-        int candidateCount = 0;
-        for (int i = 0; i < n; i++) {
-            if (candidatesForCase1.contains(i) && candidatesForCase2.contains(i)) {
+        Arrays.sort(condos, (condo1, condo2) -> {
+            if (condo1[DISTANCE] == condo2[DISTANCE]) {
+                return condo1[COST] - condo2[COST];
+            }
+            return condo1[DISTANCE] - condo2[DISTANCE];
+        });
+        int candidateCount = 1;
+        int minCost = condos[0][COST];
+        for (int i = 1; i < n; i++) {
+            if (condos[i][COST] < minCost) {
+                minCost = condos[i][COST];
                 candidateCount++;
             }
         }
         System.out.println(candidateCount);
-    }
-
-    private static List<Integer> getCandidates(int n, int[][] condos, int standard1, int standard2) {
-        Arrays.sort(condos, Comparator.comparing(condo -> condo[standard1]));
-        List<Integer> candidatesForCase = new ArrayList<>(Collections.singletonList(condos[0][NUMBER]));
-        int min = condos[0][standard2];
-        for (int i = 1; i < n; i++) {
-            int previous = condos[i - 1][standard1];
-            int currentMin = condos[i - 1][standard2];
-            while (i < n && previous == condos[i][standard1]) {
-                if (condos[i][standard2] < min) {
-                    candidatesForCase.add(condos[i][NUMBER]);
-                    currentMin = Math.min(currentMin, condos[i][standard2]);
-                }
-                i++;
-            }
-            if (i >= n) {
-                break;
-            }
-            min = Math.min(min, currentMin);
-            if (condos[i][standard2] < min) {
-                candidatesForCase.add(condos[i][NUMBER]);
-            }
-            min = Math.min(min, condos[i][standard2]);
-        }
-        return candidatesForCase;
     }
 }
