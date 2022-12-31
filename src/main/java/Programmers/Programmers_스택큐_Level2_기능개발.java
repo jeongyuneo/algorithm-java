020eab01513a83e1a13c91b1399aa7d07dc1cad9
jeonyuneo <piq2255@gmail.com>
2022-12-31 17:43:00 +0900

    [UPDATE] 기능개발 - 개선

--- src/main/java/Programmers/Programmers_스택큐_Level2_기능개발.java
+++ src/main/java/Programmers/Programmers_스택큐_Level2_기능개발.java
@@ -1,7 +1,6 @@
 package Programmers;
 
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class Programmers_스택큐_Level2_기능개발 {
 
@@ -10,38 +9,18 @@ public class Programmers_스택큐_Level2_기능개발 {
         System.out.println(Arrays.toString(solution(new int[]{95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1})));
     }
 
-    private static final int PROGRESS = 0;
-    private static final int ORDER = 1;
-
     public static int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
-        Queue<int[]> processes = new ArrayDeque<>();
-        Stack<Integer> done = new Stack<>();
-        IntStream.range(0, progresses.length).forEach(order -> processes.offer(new int[]{progresses[order], order}));
-        while (!processes.isEmpty()) {
-            int size = processes.size();
-            int order = 0;
-            while (size-- > 0) {
-                int[] process = processes.poll();
-                int progress = process[PROGRESS] + speeds[process[ORDER]];
-                if (progress >= 100 && ((done.isEmpty() && order == 0) || (!done.isEmpty() && done.peek() == order - 1))) {
-                    done.push(order++);
-                    continue;
-                }
-                processes.offer(new int[]{progress, process[ORDER]});
-                order++;
+        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> doneProgresses = new ArrayDeque<>();
+        for (int i = 0; i < progresses.length; i++) {
+            int duration = (int) Math.ceil((100 - progresses[i]) / (double) speeds[i]);
+            if (!doneProgresses.isEmpty() && doneProgresses.peek() < duration) {
+                answer.add(doneProgresses.size());
+                doneProgresses.clear();
             }
-            if (!done.isEmpty()) {
-                list.add(done.size());
-                done.clear();
-            }
+            doneProgresses.offer(duration);
         }
-
-        int[] answer = new int[list.size()];
-        int i = 0;
-        for (int number : list) {
-            answer[i++] = number;
-        }
-        return answer;
+        answer.add(doneProgresses.size());
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
