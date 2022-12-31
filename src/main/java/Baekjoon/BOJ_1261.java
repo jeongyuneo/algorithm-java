5f7f1f90c89bfe5181320b3e31e818dd6432acfb
jeonyuneo <piq2255@gmail.com>
2023-01-01 02:44:08 +0900

    [UPDATE] 알고스팟 - PriorityQueue 사용

--- src/main/java/Baekjoon/BOJ_1261.java
+++ src/main/java/Baekjoon/BOJ_1261.java
@@ -3,14 +3,14 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_1261 {
 
     private static final int[][] DELTAS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    private static final int WALL = 1;
+    private static final int BREAKING_COUNT = 2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -18,39 +18,33 @@ public class BOJ_1261 {
         int m = Integer.parseInt(stringTokenizer.nextToken());
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int[][] map = new int[n][m];
-        int[][] breakingCounts = new int[n][m];
+        boolean[][] isVisited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             String readLine = bufferedReader.readLine();
             for (int j = 0; j < m; j++) {
                 map[i][j] = readLine.charAt(j) - 48;
-                breakingCounts[i][j] = Integer.MAX_VALUE;
             }
         }
-        breakingCounts[0][0] = 0;
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{0, 0});
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparing(location -> location[BREAKING_COUNT]));
+        priorityQueue.offer(new int[]{0, 0, 0});
+        isVisited[0][0] = true;
+        while (!priorityQueue.isEmpty()) {
+            int[] current = priorityQueue.poll();
             int x = current[0];
             int y = current[1];
+            int count = current[BREAKING_COUNT];
+            if (x == n - 1 && y == m - 1) {
+                System.out.println(count);
+                break;
+            }
             for (int[] delta : DELTAS) {
                 int dx = x + delta[0];
                 int dy = y + delta[1];
-                if (dx >= 0 && dx < n && dy >= 0 && dy < m) {
-                    if (map[dx][dy] == WALL) {
-                        if (breakingCounts[dx][dy] > breakingCounts[x][y] + 1) {
-                            breakingCounts[dx][dy] = breakingCounts[x][y] + 1;
-                            queue.offer(new int[]{dx, dy});
-                        }
-                    } else {
-                        if (breakingCounts[dx][dy] > breakingCounts[x][y]) {
-                            breakingCounts[dx][dy] = breakingCounts[x][y];
-                            queue.offer(new int[]{dx, dy});
-                        }
-                    }
+                if (dx >= 0 && dx < n && dy >= 0 && dy < m && !isVisited[dx][dy]) {
+                    isVisited[dx][dy] = true;
+                    priorityQueue.offer(new int[]{dx, dy, map[dx][dy] + count});
                 }
             }
         }
-        System.out.println(breakingCounts[n - 1][m - 1]);
     }
 }
