8ab85c2118d4a43c2761e810ef327a9eac1b123b
jeonyuneo <piq2255@gmail.com>
2023-01-05 16:30:04 +0900

    [UPDATE] 프린터 - 개선

--- src/main/java/Programmers/Programmers_스택큐_Level2_프린터.java
+++ src/main/java/Programmers/Programmers_스택큐_Level2_프린터.java
@@ -1,7 +1,6 @@
 package Programmers;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 
 public class Programmers_스택큐_Level2_프린터 {
@@ -11,24 +10,38 @@ public class Programmers_스택큐_Level2_프린터 {
         System.out.println(solution(new int[]{1, 1, 9, 1, 1, 1}, 0));
     }
 
-    public static int solution(int[] priorities, int location) {
-        int order = 0;
-        Queue<Integer> printer = new ArrayDeque<>();
-        for (int priority : priorities) {
-            printer.offer(priority);
+    static class Document implements Comparable<Document> {
+
+        int location;
+        int priority;
+
+        public Document(int location, int priority) {
+            this.location = location;
+            this.priority = priority;
         }
-        Arrays.sort(priorities);
-        int documentNumber = priorities.length - 1;
+
+        @Override
+        public int compareTo(Document o) {
+            return o.priority - priority;
+        }
+    }
+
+    public static int solution(int[] priorities, int location) {
+        Queue<Document> printer = new ArrayDeque<>();
+        for (int i = 0; i < priorities.length; i++) {
+            int priority = priorities[i];
+            printer.offer(new Document(i, priority));
+        }
+        int order = 1;
         while (!printer.isEmpty()) {
-            int priority = printer.poll();
-            if (priority == priorities[documentNumber - order]) {
-                order++;
-                if (--location < 0) {
+            Document document = printer.poll();
+            if (printer.stream().anyMatch(waitingDocument -> waitingDocument.priority > document.priority)) {
+                printer.offer(document);
+            } else {
+                if (document.location == location) {
                     break;
                 }
-            } else {
-                printer.add(priority);
-                location = (location + printer.size() - 1) % printer.size();
+                order++;
             }
         }
         return order;
