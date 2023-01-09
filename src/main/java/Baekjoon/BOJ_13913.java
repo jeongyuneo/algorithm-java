4434797d6276b90a54612c4f1a011c00c24e8e1f
jeonyuneo <piq2255@gmail.com>
2023-01-09 23:45:06 +0900

    [UPDATE] 숨바꼭질 4 - 개선

--- src/main/java/Baekjoon/BOJ_13913.java
+++ src/main/java/Baekjoon/BOJ_13913.java
@@ -7,58 +7,49 @@ import java.util.*;
 
 public class BOJ_13913 {
 
-    private static final int LOCATION = 0;
-    private static final int TIME = 1;
+    private static final int NOT_VISIT = -1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int k = Integer.parseInt(stringTokenizer.nextToken());
-        PriorityQueue<int[]> subin = new PriorityQueue<>(Comparator.comparing(info -> info[TIME]));
-        subin.offer(new int[]{n, 0});
-        boolean[] isVisited = new boolean[100001];
-        int[] times = new int[100001];
-        int[] parents = new int[100001];
-        Arrays.fill(times, Integer.MAX_VALUE);
-        times[n] = 0;
-        parents[n] = -1;
+
+        Queue<Integer> subin = new ArrayDeque<>();
+        subin.offer(n);
+        int[] visited = new int[100001];
+        Arrays.fill(visited, -1);
+        visited[n] = Integer.MAX_VALUE;
+        int time = 0;
         while (!subin.isEmpty()) {
-            int[] current = subin.poll();
-            int location = current[LOCATION];
-            int time = current[TIME];
-            if (location == k) {
-                List<Integer> traces = new ArrayList<>();
-                traces.add(location);
-                int parent = parents[location];
-                while (parent > 0) {
-                    traces.add(parent);
-                    parent = parents[parent];
+            int size = subin.size();
+            while (size-- > 0) {
+                int current = subin.poll();
+                if (current == k) {
+                    Stack traces = new Stack();
+                    int parent = k;
+                    while (parent != Integer.MAX_VALUE) {
+                        traces.push(parent);
+                        parent = visited[parent];
+                    }
+                    StringBuilder answer = new StringBuilder();
+                    answer.append(time).append("\n");
+                    while (!traces.isEmpty()) {
+                        answer.append(traces.pop()).append(" ");
+                    }
+                    System.out.println(answer);
+                    break;
                 }
-                traces.add(parent);
 
-                StringBuilder answer = new StringBuilder();
-                answer.append(time).append("\n");
-                for (int i = time; i >= 0; i--) {
-                    answer.append(traces.get(i)).append(" ");
-                }
-                System.out.println(answer);
-                break;
-            }
-
-            if (isVisited[location]) {
-                continue;
-            }
-
-            isVisited[location] = true;
-            int[] nexts = {location - 1, location + 1, location * 2};
-            for (int next : nexts) {
-                if (next >= 0 && next <= 100000 && !isVisited[next] && times[next] > time + 1) {
-                    times[next] = time + 1;
-                    parents[next] = location;
-                    subin.offer(new int[]{next, time + 1});
+                int[] nexts = {current - 1, current + 1, current * 2};
+                for (int next : nexts) {
+                    if (next >= 0 && next <= 100000 && visited[next] == NOT_VISIT) {
+                        visited[next] = current;
+                        subin.offer(next);
+                    }
                 }
             }
+            time++;
         }
     }
 }
