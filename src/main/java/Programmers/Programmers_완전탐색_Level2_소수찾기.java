b71f24bccfd59eef8da0b59b69400ded9bc872ad
jeonyuneo <piq2255@gmail.com>
2023-01-11 23:22:10 +0900

    [UPDATE] 소수 찾기 - 개선

--- src/main/java/Programmers/Programmers_완전탐색_Level2_소수찾기.java
+++ src/main/java/Programmers/Programmers_완전탐색_Level2_소수찾기.java
@@ -14,41 +14,14 @@ public class Programmers_완전탐색_Level2_소수찾기 {
     private static final int MAXIMUM_SIZE = 10000000;
     private static final boolean[] IS_PRIME_NUMBER = new boolean[MAXIMUM_SIZE];
     private static final Set<Integer> NUMBERS = new HashSet<>();
-
-    private static String[] papers;
-    private static boolean[] isSelected;
-    private static String[] selectedPapers;
-    private static int paperCount;
-    private static int permutationCount;
+    private static final String EMPTY = "";
 
     public static int solution(String numbers) {
         initializePrimeNumber();
-        paperCount = numbers.length();
-        papers = numbers.split("");
-        isSelected = new boolean[paperCount];
-
-        for (permutationCount = 1; permutationCount <= paperCount; permutationCount++) {
-            selectedPapers = new String[permutationCount];
-            permutation(0);
-        }
+        permutation("", numbers);
         return (int) NUMBERS.stream()
                 .filter(number -> number == 2 || number % 2 != 0 && IS_PRIME_NUMBER[number])
                 .count();
-    }
-
-    private static void permutation(int cnt) {
-        if (cnt == permutationCount) {
-            NUMBERS.add(Integer.parseInt(String.join("", selectedPapers)));
-            return;
-        }
-        for (int i = 0; i < paperCount; i++) {
-            if (!isSelected[i]) {
-                selectedPapers[cnt] = papers[i];
-                isSelected[i] = true;
-                permutation(cnt + 1);
-                isSelected[i] = false;
-            }
-        }
     }
 
     private static void initializePrimeNumber() {
@@ -62,6 +35,16 @@ public class Programmers_완전탐색_Level2_소수찾기 {
             for (int j = 2; i * j < MAXIMUM_SIZE; j++) {
                 IS_PRIME_NUMBER[i * j] = false;
             }
+        }
+    }
+
+    private static void permutation(String permutationPapers, String restPapers) {
+        if (!permutationPapers.equals(EMPTY)) {
+            NUMBERS.add(Integer.parseInt(permutationPapers));
+        }
+
+        for (int i = 0, size = restPapers.length(); i < size; i++) {
+            permutation(permutationPapers + restPapers.charAt(i), restPapers.substring(0, i) + restPapers.substring(i + 1, size));
         }
     }
 }
