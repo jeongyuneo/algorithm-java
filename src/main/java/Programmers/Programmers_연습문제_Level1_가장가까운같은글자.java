3ac213e46432034564be443a4090d8546a0b27f1
jeonyuneo <piq2255@gmail.com>
2023-01-13 15:41:45 +0900

    [UPDATE] 가장 가까운 같은 글자 - Map 사용

--- src/main/java/Programmers/Programmers_연습문제_Level1_가장가까운같은글자.java
+++ src/main/java/Programmers/Programmers_연습문제_Level1_가장가까운같은글자.java
@@ -1,6 +1,8 @@
 package Programmers;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Programmers_연습문제_Level1_가장가까운같은글자 {
 
@@ -10,18 +12,16 @@ public class Programmers_연습문제_Level1_가장가까운같은글자 {
     }
 
     public static int[] solution(String s) {
-        int[] lastLocations = new int[26];
-        Arrays.fill(lastLocations, -1);
+        Map<Character, Integer> lastLocations = new HashMap<>();
         int[] answer = new int[s.length()];
         for (int current = 0; current < s.length(); current++) {
-            int alphabet = s.charAt(current) - 97;
-            int last = lastLocations[alphabet];
-            if (last == -1) {
-                answer[current] = -1;
+            char alphabet = s.charAt(current);
+            if (lastLocations.containsKey(alphabet)) {
+                answer[current] = current - lastLocations.get(alphabet);
             } else {
-                answer[current] = current - last;
+                answer[current] = -1;
             }
-            lastLocations[alphabet] = current;
+            lastLocations.put(alphabet, current);
         }
         return answer;
     }
