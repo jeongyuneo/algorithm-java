33e75b2adc5abd35c05aa03678b4087953848fcc
jeonyuneo <piq2255@gmail.com>
2023-01-14 18:50:25 +0900

    [UPDATE] 신입 사원 - 개선

--- src/main/java/Baekjoon/BOJ_1946.java
+++ src/main/java/Baekjoon/BOJ_1946.java
@@ -3,14 +3,9 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class BOJ_1946 {
-
-    private static final int DOCUMENT_EXAMINATION_GRADE = 0;
-    private static final int INTERVIEW_GRADE = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -18,19 +13,17 @@ public class BOJ_1946 {
         StringBuilder answer = new StringBuilder();
         while (testCase-- > 0) {
             int n = Integer.parseInt(bufferedReader.readLine());
-            int[][] candidates = new int[n][2];
+            int[] grades = new int[n];
             for (int i = 0; i < n; i++) {
                 StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-                candidates[i][DOCUMENT_EXAMINATION_GRADE] = Integer.parseInt(stringTokenizer.nextToken());
-                candidates[i][INTERVIEW_GRADE] = Integer.parseInt(stringTokenizer.nextToken());
+                grades[Integer.parseInt(stringTokenizer.nextToken()) - 1] = Integer.parseInt(stringTokenizer.nextToken());
             }
-            Arrays.sort(candidates, Comparator.comparing(newRecruit -> newRecruit[0]));
             int maxRecruit = 1;
-            int lowestGrade = candidates[0][INTERVIEW_GRADE];
+            int lowestInterviewGrade = grades[0];
             for (int i = 1; i < n; i++) {
-                if (candidates[i][INTERVIEW_GRADE] < lowestGrade) {
+                if (grades[i] < lowestInterviewGrade) {
                     maxRecruit++;
-                    lowestGrade = candidates[i][INTERVIEW_GRADE];
+                    lowestInterviewGrade = grades[i];
                 }
             }
             answer.append(maxRecruit).append("\n");
