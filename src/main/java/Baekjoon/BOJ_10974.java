0885fecc0daec3ceee7d946ec60e8dff9cd34c5e
jeonyuneo <piq2255@gmail.com>
2023-01-18 23:52:04 +0900

    [ADD] 모든 순열

---
+++ src/main/java/Baekjoon/BOJ_10974.java
@@ -0,0 +1,42 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10974 {
+
+    private static final StringBuilder ANSWER = new StringBuilder();
+
+    private static int[] selectedNumbers;
+    private static boolean[] isSelected;
+    private static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bufferedReader.readLine());
+        selectedNumbers = new int[n];
+        isSelected = new boolean[n];
+        permutation(0);
+        System.out.println(ANSWER);
+    }
+
+    private static void permutation(int cnt) {
+        if (cnt == n) {
+            for (int selectedNumber : selectedNumbers) {
+                ANSWER.append(selectedNumber).append(" ");
+            }
+            ANSWER.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!isSelected[i]) {
+                isSelected[i] = true;
+                selectedNumbers[cnt] = i + 1;
+                permutation(cnt + 1);
+                isSelected[i] = false;
+            }
+        }
+    }
+}
