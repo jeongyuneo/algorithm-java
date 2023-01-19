57788403e8c9e6375381e3146099b43cab6b19d0
jeonyuneo <piq2255@gmail.com>
2023-01-19 17:09:59 +0900

    [UPDATE] 가르침 - 개선

--- src/main/java/Baekjoon/BOJ_1062.java
+++ src/main/java/Baekjoon/BOJ_1062.java
@@ -4,56 +4,60 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.regex.Pattern;
 
 public class BOJ_1062 {
 
-    private static final String PREFIX_AND_SUFFIX = "[acnit]";
-    private static final String EMPTY = "";
-    private static final Pattern PATTERN = Pattern.compile(PREFIX_AND_SUFFIX);
+    private static final String PREFIX_AND_SUFFIX = "acnit";
 
-    private static String[] words;
+    private static int[] words;
     private static int k;
     private static int answer;
+    private static int prefixAndSuffix;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int n = Integer.parseInt(stringTokenizer.nextToken());
         k = Integer.parseInt(stringTokenizer.nextToken()) - 5;
-        answer = 0;
         if (k >= 0) {
-            words = new String[n];
+            for (int i = 0; i < 5; i++) {
+                prefixAndSuffix |= 1 << PREFIX_AND_SUFFIX.charAt(i) - 'a';
+            }
+            words = new int[n];
             for (int i = 0; i < n; i++) {
-                words[i] = bufferedReader.readLine().replaceAll(PREFIX_AND_SUFFIX, EMPTY);
+                String word = bufferedReader.readLine();
+                int wordBit = 0;
+                for (int j = 4, length = word.length() - 4; j < length; j++) {
+                    int alphabet = 1 << word.charAt(j) - 'a';
+                    if ((prefixAndSuffix & alphabet) == 0) {
+                        wordBit |= alphabet;
+                    }
+                }
+                words[i] = wordBit;
             }
             selectLearningAlphabets(0, 0, 0);
         }
         System.out.println(answer);
     }
 
-    private static void selectLearningAlphabets(int cnt, int start, int flag) {
+    private static void selectLearningAlphabets(int cnt, int start, int learningAlphabets) {
         if (cnt == k) {
-            updateMaxLearnableWords(flag);
+            updateMaxLearnableWords(learningAlphabets);
             return;
         }
 
         for (int i = start; i < 26; i++) {
-            String word = String.valueOf((char) (i + 'a'));
-            if (!PATTERN.matcher(word).matches()) {
-                selectLearningAlphabets(cnt + 1, i + 1, flag | 1 << i);
+            int alphabet = 1 << i;
+            if ((prefixAndSuffix & alphabet) == 0) {
+                selectLearningAlphabets(cnt + 1, i + 1, learningAlphabets | alphabet);
             }
         }
     }
 
-    private static void updateMaxLearnableWords(int flag) {
+    private static void updateMaxLearnableWords(int learningAlphabets) {
         int learnableWords = 0;
-        for (String word : words) {
-            int wordBit = 0;
-            for (int i = 0; i < word.length(); i++) {
-                wordBit |= 1 << word.charAt(i) - 'a';
-            }
-            if ((wordBit & flag) == wordBit) {
+        for (int word : words) {
+            if ((word & learningAlphabets) == word) {
                 learnableWords++;
             }
         }
