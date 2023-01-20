cc15ba291a37087b29232d0886ce837b600cd2b8
jeonyuneo <piq2255@gmail.com>
2023-01-20 22:01:40 +0900

    [UPDATE] 경쟁적 전염 - 개선

--- src/main/java/Baekjoon/BOJ_18405.java
+++ src/main/java/Baekjoon/BOJ_18405.java
@@ -3,8 +3,8 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_18405 {
@@ -17,19 +17,15 @@ public class BOJ_18405 {
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int k = Integer.parseInt(stringTokenizer.nextToken());
-        List<int[]>[] viruses = new List[k];
-        for (int i = 0; i < k; i++) {
-            viruses[i] = new ArrayList<>();
-        }
+        Queue<int[]> viruses = new ArrayDeque<>();
         int[][] map = new int[n][n];
-        boolean[][] isVisited = new boolean[n][n];
         for (int i = 0; i < n; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             for (int j = 0; j < n; j++) {
                 int input = Integer.parseInt(stringTokenizer.nextToken());
                 map[i][j] = input;
                 if (input != EMPTY) {
-                    viruses[input - 1].add(new int[]{i, j});
+                    viruses.offer(new int[]{i, j, input});
                 }
             }
         }
@@ -37,22 +33,19 @@ public class BOJ_18405 {
         int s = Integer.parseInt(stringTokenizer.nextToken());
         int x = Integer.parseInt(stringTokenizer.nextToken()) - 1;
         int y = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-        while (s-- > 0) {
-            for (int i = 0; i < k; i++) {
-                List<int[]> spreadingViruses = new ArrayList<>(viruses[i]);
-                viruses[i].clear();
-                for (int[] spreadingVirus : spreadingViruses) {
-                    int r = spreadingVirus[0];
-                    int c = spreadingVirus[1];
-                    isVisited[r][c] = true;
-                    for (int[] delta : DELTAS) {
-                        int dr = r + delta[0];
-                        int dc = c + delta[1];
-                        if (dr >= 0 && dr < n && dc >= 0 && dc < n && !isVisited[dr][dc] && map[dr][dc] == EMPTY) {
-                            isVisited[dr][dc] = true;
-                            map[dr][dc] = map[r][c];
-                            viruses[i].add(new int[]{dr, dc});
-                        }
+        while (s-- > 0 && map[x][y] == EMPTY) {
+            int size = viruses.size();
+            while (size-- > 0) {
+                int[] current = viruses.poll();
+                int r = current[0];
+                int c = current[1];
+                int type = current[2];
+                for (int[] delta : DELTAS) {
+                    int dr = r + delta[0];
+                    int dc = c + delta[1];
+                    if (dr >= 0 && dr < n && dc >= 0 && dc < n && (map[dr][dc] == EMPTY || map[dr][dc] > type)) {
+                        map[dr][dc] = type;
+                        viruses.offer(new int[]{dr, dc, type});
                     }
                 }
             }
