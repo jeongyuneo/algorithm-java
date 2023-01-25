757433d77204815a235831dc5bc3e8be93a41f24
jeonyuneo <piq2255@gmail.com>
2023-01-25 14:09:14 +0900

    [UPDATE] 계단 오르기 - 개선

--- src/main/java/Baekjoon/BOJ_2579.java
+++ src/main/java/Baekjoon/BOJ_2579.java
@@ -6,9 +6,6 @@ import java.io.InputStreamReader;
 
 public class BOJ_2579 {
 
-    private static final int ONE_STEP = 0;
-    private static final int TWO_STEP = 1;
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
@@ -16,13 +13,14 @@ public class BOJ_2579 {
         for (int i = 1; i <= n; i++) {
             stairs[i] = Integer.parseInt(bufferedReader.readLine());
         }
-        int[][] scores = new int[n + 1][2];
-        scores[1][ONE_STEP] = stairs[1];
-        scores[1][TWO_STEP] = stairs[1];
-        for (int i = 2; i <= n; i++) {
-            scores[i][ONE_STEP] = scores[i - 1][TWO_STEP] + stairs[i];
-            scores[i][TWO_STEP] = Math.max(scores[i - 2][ONE_STEP], scores[i - 2][TWO_STEP]) + stairs[i];
+        int[] scores = new int[n + 1];
+        scores[1] = stairs[1];
+        if (n > 1) {
+            scores[2] = stairs[1] + stairs[2];
+            for (int i = 3; i <= n; i++) {
+                scores[i] = Math.max(scores[i - 3] + stairs[i - 1], scores[i - 2]) + stairs[i];
+            }
         }
-        System.out.println(Math.max(scores[n][ONE_STEP], scores[n][TWO_STEP]));
+        System.out.println(scores[n]);
     }
 }
