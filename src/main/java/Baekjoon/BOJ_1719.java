51e29db9307b94d434ff2818e09f9d4a5cb15e97
jeonyuneo <piq2255@gmail.com>
2023-01-28 21:43:47 +0900

    [UPDATE] 택배 - 다익스트라 풀이

--- src/main/java/Baekjoon/BOJ_1719.java
+++ src/main/java/Baekjoon/BOJ_1719.java
@@ -3,55 +3,91 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_1719 {
 
-    private static final int MAX_VALUE = 100000000;
+    private static final StringBuilder ANSWER = new StringBuilder();
+    private static final int TO = 0;
+    private static final int TIME = 1;
+    private static final PriorityQueue<int[]> PRIORITY_QUEUE = new PriorityQueue<>(Comparator.comparing(info -> info[TIME]));
+
+    private static List<int[]>[] adjList;
+    private static int[] times;
+    private static int[] paths;
+    private static boolean[] isVisited;
+    private static int n;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int n = Integer.parseInt(stringTokenizer.nextToken());
+        n = Integer.parseInt(stringTokenizer.nextToken());
         int m = Integer.parseInt(stringTokenizer.nextToken());
-        int[][] map = new int[n][n];
-        int[][] paths = new int[n][n];
+        adjList = new List[n];
+        times = new int[n];
+        paths = new int[n];
+        isVisited = new boolean[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                map[i][j] = MAX_VALUE;
-                paths[i][j] = j;
-            }
+            adjList[i] = new ArrayList<>();
         }
         for (int i = 0; i < m; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int from = Integer.parseInt(stringTokenizer.nextToken()) - 1;
             int to = Integer.parseInt(stringTokenizer.nextToken()) - 1;
             int time = Integer.parseInt(stringTokenizer.nextToken());
-            map[from][to] = time;
-            map[to][from] = time;
-        }
-        for (int pass = 0; pass < n; pass++) {
-            for (int start = 0; start < n; start++) {
-                for (int end = 0; end < n; end++) {
-                    if (map[start][end] > map[start][pass] + map[pass][end]) {
-                        map[start][end] = map[start][pass] + map[pass][end];
-                        paths[start][end] = paths[start][pass];
-                    }
-                }
-            }
+            adjList[from].add(new int[]{to, time});
+            adjList[to].add(new int[]{from, time});
         }
 
-        StringBuilder answer = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    answer.append("- ");
-                    continue;
-                }
-                answer.append(paths[i][j] + 1).append(" ");
-            }
-            answer.append("\n");
+            PRIORITY_QUEUE.clear();
+            Arrays.fill(times, Integer.MAX_VALUE);
+            Arrays.fill(paths, 0);
+            Arrays.fill(isVisited, false);
+            findMinDistance(i);
         }
-        System.out.println(answer);
+        System.out.println(ANSWER);
+    }
+
+    private static void findMinDistance(int start) {
+        PRIORITY_QUEUE.offer(new int[]{start, 0});
+        times[start] = 0;
+        paths[start] = start;
+        while (!PRIORITY_QUEUE.isEmpty()) {
+            int[] current = PRIORITY_QUEUE.poll();
+            int to = current[TO];
+            if (isVisited[to]) {
+                continue;
+            }
+            isVisited[to] = true;
+            for (int[] next : adjList[to]) {
+                int nextTo = next[TO];
+                int nextTime = times[to] + next[TIME];
+                if (times[nextTo] > nextTime) {
+                    times[nextTo] = nextTime;
+                    paths[nextTo] = to;
+                    PRIORITY_QUEUE.offer(new int[]{nextTo, nextTime});
+                }
+            }
+        }
+        updateAnswer(start);
+    }
+
+    private static void updateAnswer(int start) {
+        for (int i = 0; i < n; i++) {
+            if (i == start) {
+                ANSWER.append("- ");
+                continue;
+            }
+            ANSWER.append(findFirstPath(start, i)).append(" ");
+        }
+        ANSWER.append("\n");
+    }
+
+    private static int findFirstPath(int start, int end) {
+        if (paths[end] == start) {
+            return end + 1;
+        }
+        return findFirstPath(start, paths[end]);
     }
 }
