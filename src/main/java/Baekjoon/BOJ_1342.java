19cedf34217d86e06da1cf1887e91337f0b3593a
jeonyuneo <piq2255@gmail.com>
2023-01-29 17:21:07 +0900

    [UPDATE] 비밀 모임 - 다익스트라 풀이

--- src/main/java/Baekjoon/BOJ_1342.java
+++ src/main/java/Baekjoon/BOJ_1342.java
@@ -3,11 +3,17 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_1342 {
 
-    private static final int MAX_VALUE = 1000000;
+    private static final int NUMBER = 0;
+    private static final int DISTANCE = 1;
+    private static final PriorityQueue<int[]> PRIORITY_QUEUE = new PriorityQueue<>(Comparator.comparing(info -> info[DISTANCE]));
+
+    private static int[] distances;
+    private static boolean[] isVisited;
+    private static int[] totalDistances;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -17,49 +23,71 @@ public class BOJ_1342 {
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int n = Integer.parseInt(stringTokenizer.nextToken());
             int m = Integer.parseInt(stringTokenizer.nextToken());
-            int[][] rooms = new int[n][n];
+            List<int[]>[] rooms = new List[n];
+            distances = new int[n];
+            isVisited = new boolean[n];
+            totalDistances = new int[n];
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (i != j) {
-                        rooms[i][j] = MAX_VALUE;
-                    }
-                }
+                rooms[i] = new ArrayList<>();
             }
             for (int i = 0; i < m; i++) {
                 stringTokenizer = new StringTokenizer(bufferedReader.readLine());
                 int from = Integer.parseInt(stringTokenizer.nextToken()) - 1;
                 int to = Integer.parseInt(stringTokenizer.nextToken()) - 1;
                 int distance = Integer.parseInt(stringTokenizer.nextToken());
-                rooms[from][to] = distance;
-                rooms[to][from] = distance;
-            }
-            for (int pass = 0; pass < n; pass++) {
-                for (int start = 0; start < n; start++) {
-                    for (int end = 0; end < n; end++) {
-                        rooms[start][end] = Math.min(rooms[start][end], rooms[start][pass] + rooms[pass][end]);
-                    }
-                }
+                rooms[from].add(new int[]{to, distance});
+                rooms[to].add(new int[]{from, distance});
             }
             int k = Integer.parseInt(bufferedReader.readLine());
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int[] friends = new int[k];
             for (int i = 0; i < k; i++) {
-                friends[i] = Integer.parseInt(stringTokenizer.nextToken()) - 1;
+                PRIORITY_QUEUE.clear();
+                Arrays.fill(distances, Integer.MAX_VALUE);
+                Arrays.fill(isVisited, false);
+                findMinDistance(rooms, n, Integer.parseInt(stringTokenizer.nextToken()) - 1);
             }
-            int minDistance = Integer.MAX_VALUE;
-            int meetingRoom = n;
-            for (int i = 0; i < n; i++) {
-                int totalDistance = 0;
-                for (int j = 0; j < k; j++) {
-                    totalDistance += rooms[i][friends[j]];
-                }
-                if (minDistance > totalDistance) {
-                    minDistance = totalDistance;
-                    meetingRoom = i + 1;
-                }
-            }
-            answer.append(meetingRoom).append("\n");
+            answer.append(getMeetingRoom(n)).append("\n");
         }
         System.out.println(answer);
+    }
+
+    private static void findMinDistance(List<int[]>[] rooms, int n, int start) {
+        PRIORITY_QUEUE.offer(new int[]{start, 0});
+        distances[start] = 0;
+        while (!PRIORITY_QUEUE.isEmpty()) {
+            int[] current = PRIORITY_QUEUE.poll();
+            int from = current[NUMBER];
+            if (isVisited[from]) {
+                continue;
+            }
+            isVisited[from] = true;
+            for (int[] next : rooms[from]) {
+                int to = next[NUMBER];
+                int distance = distances[from] + next[DISTANCE];
+                if (distances[to] > distance) {
+                    distances[to] = distance;
+                    PRIORITY_QUEUE.offer(new int[]{to, distance});
+                }
+            }
+        }
+        sumDistances(n);
+    }
+
+    private static void sumDistances(int n) {
+        for (int i = 0; i < n; i++) {
+            totalDistances[i] += distances[i];
+        }
+    }
+
+    private static int getMeetingRoom(int n) {
+        int minDistance = Integer.MAX_VALUE;
+        int meetingRoom = 0;
+        for (int i = 0; i < n; i++) {
+            if (minDistance > totalDistances[i]) {
+                minDistance = totalDistances[i];
+                meetingRoom = i + 1;
+            }
+        }
+        return meetingRoom;
     }
 }
