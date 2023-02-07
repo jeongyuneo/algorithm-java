dbc049401c8e3988e951492262142567f5f66490
jeonyuneo <piq2255@gmail.com>
2023-02-07 12:18:17 +0900

    [UPDATE] 레이저 통신 - 재풀이

--- src/main/java/Baekjoon/BOJ_6087.java
+++ src/main/java/Baekjoon/BOJ_6087.java
@@ -3,89 +3,84 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_6087 {
 
     private static final int[][] DELTAS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private static final PriorityQueue<int[]> LASER = new PriorityQueue<>(Comparator.comparing(info -> info[3]));
     private static final char C = 'C';
     private static final char WALL = '*';
 
-    static class Laser {
-
-        int x;
-        int y;
-        int direction;
-        int mirrorCount;
-
-        public Laser(int x, int y, int direction, int mirrorCount) {
-            this.x = x;
-            this.y = y;
-            this.direction = direction;
-            this.mirrorCount = mirrorCount;
-        }
-    }
+    private static int width;
+    private static int height;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int width = Integer.parseInt(stringTokenizer.nextToken());
-        int height = Integer.parseInt(stringTokenizer.nextToken());
+        width = Integer.parseInt(stringTokenizer.nextToken());
+        height = Integer.parseInt(stringTokenizer.nextToken());
         char[][] map = new char[height][width];
-        Laser start = null;
-        int[][] minimumMirrorCounts = new int[height][width];
-        for (int x = 0; x < height; x++) {
-            String inputLine = bufferedReader.readLine();
-            for (int y = 0; y < width; y++) {
-                minimumMirrorCounts[x][y] = Integer.MAX_VALUE;
-                char input = inputLine.charAt(y);
-                map[x][y] = input;
-                if (input == C && start == null) {
-                    minimumMirrorCounts[x][y] = 0;
-                    start = new Laser(x, y, 0, 0);
+        int[][] mirrors = new int[height][width];
+        int[] initC = new int[]{-1, -1};
+        for (int i = 0; i < height; i++) {
+            Arrays.fill(mirrors[i], Integer.MAX_VALUE);
+            String line = bufferedReader.readLine();
+            for (int j = 0; j < width; j++) {
+                char input = line.charAt(j);
+                map[i][j] = input;
+                if (input == C && initC[0] == -1) {
+                    initC[0] = i;
+                    initC[1] = j;
+                    mirrors[i][j] = 0;
                 }
             }
         }
-
-        Queue<Laser> laser = new ArrayDeque<>();
-        for (int direction = 0, deltaLength = DELTAS.length; direction < deltaLength; direction++) {
-            int dx = start.x + DELTAS[direction][0];
-            int dy = start.y + DELTAS[direction][1];
-            if (isInBoundary(width, height, dx, dy) && map[dx][dy] != WALL) {
-                laser.offer(new Laser(dx, dy, direction, 0));
-                minimumMirrorCounts[dx][dy] = 0;
+        for (int direction = 0; direction < DELTAS.length; direction++) {
+            int[] delta = DELTAS[direction];
+            int dx = initC[0] + delta[0];
+            int dy = initC[1] + delta[1];
+            if (isInBoundary(dx, dy) && map[dx][dy] != WALL) {
+                LASER.offer(new int[]{dx, dy, direction, 0});
+                mirrors[dx][dy] = 0;
             }
         }
-
-        int minimumMirrorCount = Integer.MAX_VALUE;
-        while (!laser.isEmpty()) {
-            Laser current = laser.poll();
-            int x = current.x;
-            int y = current.y;
-            int direction = current.direction;
-            int mirrorCount = current.mirrorCount;
-
-            if (map[x][y] == C) {
-                minimumMirrorCount = Math.min(minimumMirrorCount, mirrorCount);
-            }
-
-            for (int i = -1; i <= 1; i++) {
-                int nextDirection = (direction + i + DELTAS.length) % DELTAS.length;
-                int dx = x + DELTAS[nextDirection][0];
-                int dy = y + DELTAS[nextDirection][1];
-                int nextMirrorCount = mirrorCount + Math.abs(i);
-                if (isInBoundary(width, height, dx, dy) && map[dx][dy] != WALL && minimumMirrorCounts[dx][dy] >= nextMirrorCount) {
-                    laser.offer(new Laser(dx, dy, nextDirection, nextMirrorCount));
-                    minimumMirrorCounts[dx][dy] = nextMirrorCount;
-                }
-            }
-        }
-        System.out.println(minimumMirrorCount);
+        System.out.println(getMinMirrorCount(map, mirrors));
     }
 
-    private static boolean isInBoundary(int width, int height, int dx, int dy) {
-        return dx >= 0 && dx < height && dy >= 0 && dy < width;
+    private static int getMinMirrorCount(char[][] map, int[][] mirrors) {
+        while (!LASER.isEmpty()) {
+            int[] current = LASER.poll();
+            int x = current[0];
+            int y = current[1];
+            int mirror = current[3];
+            if (map[x][y] == C) {
+                return mirror;
+            }
+            int direction = current[2];
+            for (int nextDirection = 0; nextDirection < DELTAS.length; nextDirection++) {
+                int dx = x + DELTAS[nextDirection][0];
+                int dy = y + DELTAS[nextDirection][1];
+                if (isInBoundary(dx, dy) && map[dx][dy] != WALL) {
+                    if (direction == nextDirection) {
+                        mirrors[dx][dy] = mirror;
+                        LASER.offer(new int[]{dx, dy, nextDirection, mirror});
+                    } else {
+                        if (mirrors[dx][dy] > mirror + 1) {
+                            mirrors[dx][dy] = mirror + 1;
+                            LASER.offer(new int[]{dx, dy, nextDirection, mirror + 1});
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static boolean isInBoundary(int x, int y) {
+        return x >= 0 && x < height && y >= 0 && y < width;
     }
 }
