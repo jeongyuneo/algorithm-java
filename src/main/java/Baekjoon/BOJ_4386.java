3235d305266e30ae703d735ec1279d51e3b7808d
jeonyuneo <piq2255@gmail.com>
2023-02-10 17:16:54 +0900

    [UPDATE] 별자리 만들기 - 프림 알고리즘

--- src/main/java/Baekjoon/BOJ_4386.java
+++ src/main/java/Baekjoon/BOJ_4386.java
@@ -7,19 +7,14 @@ import java.util.*;
 
 public class BOJ_4386 {
 
-    private static final int FROM = 0;
-    private static final int TO = 1;
-    private static final int COST = 2;
-
-    private static int[] parents;
+    private static final int NUMBER = 0;
+    private static final int COST = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
         double[][] stars = new double[n][n];
-        parents = new int[n];
         for (int i = 0; i < n; i++) {
-            parents[i] = i;
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             stars[i][0] = Double.parseDouble(stringTokenizer.nextToken());
             stars[i][1] = Double.parseDouble(stringTokenizer.nextToken());
@@ -33,48 +28,43 @@ public class BOJ_4386 {
         System.out.printf("%.2f", getMinCost(n, getLines(n, stars)));
     }
 
-    private static List<double[]> getLines(int n, double[][] stars) {
-        List<double[]> lines = new ArrayList<>();
+    private static List<double[]>[] getLines(int n, double[][] stars) {
+        List<double[]>[] lines = new List[n];
+        for (int i = 0; i < n; i++) {
+            lines[i] = new ArrayList<>();
+        }
         for (int from = 0; from < n; from++) {
             for (int to = from + 1; to < n; to++) {
-                lines.add(new double[]{from, to, Math.sqrt(Math.pow(stars[to][0] - stars[from][0], 2) + Math.pow(stars[to][1] - stars[from][1], 2))});
+                lines[from].add(new double[]{to, Math.sqrt(Math.pow(stars[to][0] - stars[from][0], 2) + Math.pow(stars[to][1] - stars[from][1], 2))});
+                lines[to].add(new double[]{from, Math.sqrt(Math.pow(stars[to][0] - stars[from][0], 2) + Math.pow(stars[to][1] - stars[from][1], 2))});
             }
         }
-        lines.sort(Comparator.comparing(line -> line[COST]));
         return lines;
     }
 
-    private static double getMinCost(int n, List<double[]> lines) {
+    private static double getMinCost(int n, List<double[]>[] lines) {
+        PriorityQueue<double[]> priorityQueue = new PriorityQueue<>(Comparator.comparing(info -> info[COST]));
+        boolean[] isVisited = new boolean[n];
+        priorityQueue.offer(new double[]{0, 0});
         double cost = 0;
-        int combinedCount = 1;
-        for (double[] line : lines) {
-            if (canCombine((int) line[FROM], (int) line[TO])) {
-                cost += line[COST];
-                if (++combinedCount == n) {
-                    break;
+        int combinedCount = 0;
+        while (!priorityQueue.isEmpty()) {
+            double[] current = priorityQueue.poll();
+            int from = (int) current[NUMBER];
+            if (isVisited[from]) {
+                continue;
+            }
+            cost += current[COST];
+            if (++combinedCount == n) {
+                break;
+            }
+            isVisited[from] = true;
+            for (double[] next : lines[from]) {
+                if (!isVisited[(int) next[NUMBER]]) {
+                    priorityQueue.offer(next);
                 }
             }
         }
         return cost;
-    }
-
-    private static boolean canCombine(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-        if (rootA == rootB) {
-            return false;
-        } else if (rootA > rootB) {
-            parents[rootA] = rootB;
-        } else {
-            parents[rootB] = rootA;
-        }
-        return true;
-    }
-
-    private static int find(int value) {
-        if (parents[value] == value) {
-            return value;
-        }
-        return parents[value] = find(parents[value]);
     }
 }
