2521008ae8e6cddf2cb4060b89a843624084778a
jeonyuneo <piq2255@gmail.com>
2023-02-10 16:56:48 +0900

    [UPDATE] 최소 스패닝 트리 - 프림 알고리즘

--- src/main/java/Baekjoon/BOJ_1197.java
+++ src/main/java/Baekjoon/BOJ_1197.java
@@ -3,68 +3,52 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_1197 {
 
-    private static final int FROM = 0;
-    private static final int TO = 1;
-    private static final int WEIGHT = 2;
-
-    private static int[][] edges;
-    private static int[] parents;
+    private static final int NUMBER = 0;
+    private static final int WEIGHT = 1;
+    private static final PriorityQueue<int[]> PRIORITY_QUEUE = new PriorityQueue<>(Comparator.comparing(edge -> edge[WEIGHT]));
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int v = Integer.parseInt(stringTokenizer.nextToken());
         int e = Integer.parseInt(stringTokenizer.nextToken());
-        edges = new int[e][3];
-        parents = new int[v];
+        List<int[]>[] edges = new List[v];
         for (int i = 0; i < v; i++) {
-            parents[i] = i;
+            edges[i] = new ArrayList<>();
         }
         for (int i = 0; i < e; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            edges[i] = new int[]{Integer.parseInt(stringTokenizer.nextToken()) - 1, Integer.parseInt(stringTokenizer.nextToken()) - 1, Integer.parseInt(stringTokenizer.nextToken())};
+            int from = Integer.parseInt(stringTokenizer.nextToken()) - 1;
+            int to = Integer.parseInt(stringTokenizer.nextToken()) - 1;
+            int weight = Integer.parseInt(stringTokenizer.nextToken());
+            edges[from].add(new int[]{to, weight});
+            edges[to].add(new int[]{from, weight});
         }
-        Arrays.sort(edges, Comparator.comparing(edge -> edge[WEIGHT]));
-        System.out.println(getMinWeight(v));
-    }
-
-    private static int getMinWeight(int v) {
-        int combinedCount = 1;
+        boolean[] isVisited = new boolean[v];
         int weight = 0;
-        for (int[] edge : edges) {
-            if (canCombine(edge[FROM], edge[TO])) {
-                weight += edge[WEIGHT];
-                if (++combinedCount == v) {
-                    return weight;
+        int combinedCount = 0;
+        PRIORITY_QUEUE.offer(new int[]{0, 0});
+        while (!PRIORITY_QUEUE.isEmpty()) {
+            int[] current = PRIORITY_QUEUE.poll();
+            int from = current[NUMBER];
+            if (isVisited[from]) {
+                continue;
+            }
+            weight += current[WEIGHT];
+            if (++combinedCount == v) {
+                break;
+            }
+            isVisited[from] = true;
+            for (int[] next : edges[from]) {
+                if (!isVisited[next[NUMBER]]) {
+                    PRIORITY_QUEUE.offer(next);
                 }
             }
         }
-        return weight;
-    }
-
-    private static boolean canCombine(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-        if (rootA == rootB) {
-            return false;
-        } else if (rootA > rootB) {
-            parents[rootA] = rootB;
-        } else {
-            parents[rootB] = rootA;
-        }
-        return true;
-    }
-
-    private static int find(int value) {
-        if (parents[value] == value) {
-            return value;
-        }
-        return parents[value] = find(parents[value]);
+        System.out.println(weight);
     }
 }
