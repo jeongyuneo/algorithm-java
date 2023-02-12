b4436adab68324503cf60a8e99c1c8f0e70ad2e7
jeonyuneo <piq2255@gmail.com>
2023-02-12 22:02:35 +0900

    [UPDATE] 네트워크 연결 - 크루스칼 알고리즘

--- src/main/java/Baekjoon/BOJ_1922.java
+++ src/main/java/Baekjoon/BOJ_1922.java
@@ -3,53 +3,63 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class BOJ_1922 {
 
-    private static final int NUMBER = 0;
-    private static final int COST = 1;
-    private static final PriorityQueue<int[]> PRIORITY_QUEUE = new PriorityQueue<>(Comparator.comparing(computer -> computer[COST]));
+    private static final int FROM = 0;
+    private static final int TO = 1;
+    private static final int COST = 2;
+
+    private static int[] roots;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
         int m = Integer.parseInt(bufferedReader.readLine());
-        List<int[]>[] computers = new List[n];
-        boolean[] isVisited = new boolean[n];
+        roots = new int[n];
         for (int i = 0; i < n; i++) {
-            computers[i] = new ArrayList<>();
+            roots[i] = i;
         }
+        List<int[]> computers = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int from = Integer.parseInt(stringTokenizer.nextToken()) - 1;
             int to = Integer.parseInt(stringTokenizer.nextToken()) - 1;
             int cost = Integer.parseInt(stringTokenizer.nextToken());
-            computers[from].add(new int[]{to, cost});
-            if (from != to) {
-                computers[to].add(new int[]{from, cost});
+            computers.add(new int[]{from, to, cost});
+        }
+        computers.sort(Comparator.comparing(computer -> computer[COST]));
+        System.out.println(getCost(computers));
+    }
+
+    private static int getCost(List<int[]> computers) {
+        int cost = 0;
+        for (int[] computer : computers) {
+            if (canCombine(computer[FROM], computer[TO])) {
+                cost += computer[COST];
             }
         }
-        int minCost = 0;
-        int combinedCount = 0;
-        PRIORITY_QUEUE.offer(new int[]{0, 0});
-        while (!PRIORITY_QUEUE.isEmpty()) {
-            int[] current = PRIORITY_QUEUE.poll();
-            int from = current[NUMBER];
-            if (isVisited[from]) {
-                continue;
-            }
-            minCost += current[COST];
-            if (++combinedCount == n) {
-                break;
-            }
-            isVisited[from] = true;
-            for (int[] next : computers[from]) {
-                if (!isVisited[next[NUMBER]]) {
-                    PRIORITY_QUEUE.offer(next);
-                }
-            }
+        return cost;
+    }
+
+    private static boolean canCombine(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA == rootB) {
+            return false;
         }
-        System.out.println(minCost);
+        roots[rootB] = rootA;
+        return true;
+    }
+
+    private static int find(int value) {
+        if (roots[value] == value) {
+            return value;
+        }
+        return roots[value] = find(roots[value]);
     }
 }
