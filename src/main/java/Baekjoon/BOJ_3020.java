1dc8e216f56d43127d5b7b9c3854892ce35d6b9c
jeonyuneo <piq2255@gmail.com>
2023-02-13 17:12:31 +0900

    [UPDATE] 개똥벌레 - 이분 탐색 사용

--- src/main/java/Baekjoon/BOJ_3020.java
+++ src/main/java/Baekjoon/BOJ_3020.java
@@ -3,6 +3,7 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_3020 {
@@ -12,24 +13,19 @@ public class BOJ_3020 {
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int h = Integer.parseInt(stringTokenizer.nextToken());
-        int[] floor = new int[h];
-        int[] ceiling = new int[h];
-        for (int i = 0; i < n; i++) {
-            int length = Integer.parseInt(bufferedReader.readLine()) - 1;
-            if (i % 2 == 0) {
-                floor[length]++;
-            } else {
-                ceiling[length]++;
-            }
+        int half = n / 2;
+        int[] floor = new int[half];
+        int[] ceiling = new int[half];
+        for (int i = 0; i < half; i++) {
+            floor[i] = Integer.parseInt(bufferedReader.readLine());
+            ceiling[i] = Integer.parseInt(bufferedReader.readLine());
         }
-        for (int i = h - 2; i >= 0; i--) {
-            floor[i] += floor[i + 1];
-            ceiling[i] += ceiling[i + 1];
-        }
+        Arrays.sort(floor);
+        Arrays.sort(ceiling);
         int obstacle = n;
         int section = 0;
-        for (int i = 0; i < h; i++) {
-            int breakingObstacle = floor[i] + ceiling[h - i - 1];
+        for (int i = 1; i <= h; i++) {
+            int breakingObstacle = getBreakingObstacle(i, floor) + getBreakingObstacle(h + 1 - i, ceiling);
             if (breakingObstacle < obstacle) {
                 obstacle = breakingObstacle;
                 section = 1;
@@ -38,5 +34,19 @@ public class BOJ_3020 {
             }
         }
         System.out.println(obstacle + " " + section);
+    }
+
+    private static int getBreakingObstacle(int h, int[] cave) {
+        int start = 0;
+        int end = cave.length;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (cave[mid] >= h) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return cave.length - start;
     }
 }
