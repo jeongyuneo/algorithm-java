c2ac51ca5017ca53e66d23547e60a0dc6487ac66
jeonyuneo <piq2255@gmail.com>
2023-05-29 17:27:19 +0900

    [UPDATE] 게임 맵 최단거리 - Queue 사용

--- src/main/java/Programmers/Programmers_깊이너비우선탐색_Level2_게임맵최단거리.java
+++ src/main/java/Programmers/Programmers_깊이너비우선탐색_Level2_게임맵최단거리.java
@@ -1,7 +1,7 @@
 package Programmers;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Programmers_깊이너비우선탐색_Level2_게임맵최단거리 {
 
@@ -12,32 +12,35 @@ public class Programmers_깊이너비우선탐색_Level2_게임맵최단거리 {
 
     private static final int[][] DELTAS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     private static final int WALL = 0;
-    private static final int MOVE = 2;
-    private static final PriorityQueue<int[]> MOVES = new PriorityQueue<>(Comparator.comparing(info -> info[MOVE]));
+    private static final Queue<int[]> MOVES = new ArrayDeque<>();
 
     public static int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
         MOVES.clear();
         boolean[][] isVisited = new boolean[n][m];
-        MOVES.offer(new int[]{0, 0, 1});
+        MOVES.offer(new int[]{0, 0});
+        int move = 0;
         while (!MOVES.isEmpty()) {
-            int[] current = MOVES.poll();
-            int x = current[0];
-            int y = current[1];
-            int move = current[MOVE];
-            if (x == n - 1 && y == m - 1) {
-                return move;
-            }
-            if (isVisited[x][y]) {
-                continue;
-            }
-            isVisited[x][y] = true;
-            for (int[] delta : DELTAS) {
-                int dx = x + delta[0];
-                int dy = y + delta[1];
-                if (dx >= 0 && dx < n && dy >= 0 && dy < m && !isVisited[dx][dy] && maps[dx][dy] != WALL) {
-                    MOVES.offer(new int[]{dx, dy, move + 1});
+            int size = MOVES.size();
+            move++;
+            while (size-- > 0) {
+                int[] current = MOVES.poll();
+                int x = current[0];
+                int y = current[1];
+                if (x == n - 1 && y == m - 1) {
+                    return move;
+                }
+                if (isVisited[x][y]) {
+                    continue;
+                }
+                isVisited[x][y] = true;
+                for (int[] delta : DELTAS) {
+                    int dx = x + delta[0];
+                    int dy = y + delta[1];
+                    if (dx >= 0 && dx < n && dy >= 0 && dy < m && !isVisited[dx][dy] && maps[dx][dy] != WALL) {
+                        MOVES.offer(new int[]{dx, dy});
+                    }
                 }
             }
         }
