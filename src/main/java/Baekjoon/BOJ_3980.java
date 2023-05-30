307830f524df1869f4af57df3acb3a946835a526
jeonyuneo <piq2255@gmail.com>
2023-05-31 02:05:55 +0900

    [UPDATE] 선발 명단 - 능력치 이차원 배열 저장

--- src/main/java/Baekjoon/BOJ_3980.java
+++ src/main/java/Baekjoon/BOJ_3980.java
@@ -3,15 +3,13 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_3980 {
 
     private static final int PLAYER_COUNT = 11;
-    private static final int POSITION = 1;
-    private static final int ABILITY = 2;
+    private static final int[][] ABILITIES = new int[PLAYER_COUNT][PLAYER_COUNT];
     private static final boolean[] IS_SELECTED = new boolean[PLAYER_COUNT];
 
     private static int max;
@@ -21,37 +19,38 @@ public class BOJ_3980 {
         int testCase = Integer.parseInt(bufferedReader.readLine());
         StringBuilder answer = new StringBuilder();
         while (testCase-- > 0) {
-            List<int[]>[] abilities = new List[PLAYER_COUNT];
-            for (int i = 0; i < PLAYER_COUNT; i++) {
-                abilities[i] = new ArrayList<>();
-            }
             for (int i = 0; i < PLAYER_COUNT; i++) {
                 StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
                 for (int j = 0; j < PLAYER_COUNT; j++) {
-                    int ability = Integer.parseInt(stringTokenizer.nextToken());
-                    if (ability != 0) {
-                        abilities[i].add(new int[]{i, j, ability});
-                    }
+                    ABILITIES[i][j] = Integer.parseInt(stringTokenizer.nextToken());
                 }
             }
             max = 0;
-            selectPositions(abilities, 0, 0);
+            selectPositions(ABILITIES, 0, 0);
             answer.append(max).append("\n");
+            initialize();
         }
         System.out.println(answer);
     }
 
-    private static void selectPositions(List<int[]>[] abilities, int player, int totalAbility) {
+    private static void selectPositions(int[][] abilities, int player, int totalAbility) {
         if (player == PLAYER_COUNT) {
             max = Math.max(max, totalAbility);
             return;
         }
-        for (int[] ability : abilities[player]) {
-            if (!IS_SELECTED[ability[POSITION]]) {
-                IS_SELECTED[ability[POSITION]] = true;
-                selectPositions(abilities, player + 1, totalAbility + ability[ABILITY]);
-                IS_SELECTED[ability[POSITION]] = false;
+        for (int position = 0; position < PLAYER_COUNT; position++) {
+            int ability = abilities[player][position];
+            if (ability > 0 && !IS_SELECTED[position]) {
+                IS_SELECTED[position] = true;
+                selectPositions(abilities, player + 1, totalAbility + ability);
+                IS_SELECTED[position] = false;
             }
+        }
+    }
+
+    private static void initialize() {
+        for (int i = 0; i < PLAYER_COUNT; i++) {
+            Arrays.fill(ABILITIES[i], 0);
         }
     }
 }
