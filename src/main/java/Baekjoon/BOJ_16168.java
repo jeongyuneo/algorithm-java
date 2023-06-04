250d318c4142fd050b5d69dc478a9d79d4341ce6
jeonyuneo <piq2255@gmail.com>
2023-06-04 18:04:42 +0900

    [UPDATE] 퍼레이드 - 개선

--- src/main/java/Baekjoon/BOJ_16168.java
+++ src/main/java/Baekjoon/BOJ_16168.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -11,7 +12,7 @@ public class BOJ_16168 {
 
     private static int edge;
     private static List<Integer>[] edges;
-    private static int[][] circuit;
+    private static boolean[][] isPassed;
     private static String result;
     private static boolean isConnected;
 
@@ -21,7 +22,7 @@ public class BOJ_16168 {
         int vertex = Integer.parseInt(stringTokenizer.nextToken());
         edge = Integer.parseInt(stringTokenizer.nextToken());
         edges = new List[vertex];
-        circuit = new int[vertex][vertex];
+        isPassed = new boolean[vertex][vertex];
         for (int i = 0; i < vertex; i++) {
             edges[i] = new ArrayList<>();
         }
@@ -34,27 +35,30 @@ public class BOJ_16168 {
         }
         result = "NO";
         for (int i = 0; i < vertex; i++) {
-            canConnected(i, i, 0);
+            canConnected(i, 0);
             if (isConnected) {
                 break;
+            }
+            for (int j = 0; j < vertex; j++) {
+                Arrays.fill(isPassed[j], false);
             }
         }
         System.out.println(result);
     }
 
-    private static void canConnected(int from, int number, int cnt) {
+    private static void canConnected(int from, int cnt) {
         if (cnt == edge) {
             result = "YES";
             isConnected = true;
             return;
         }
         for (int to : edges[from]) {
-            if (circuit[from][to] == number || circuit[to][from] == number) {
+            if (isPassed[from][to] || isPassed[to][from]) {
                 continue;
             }
-            circuit[from][to] = number;
-            circuit[to][from] = number;
-            canConnected(to, number, cnt + 1);
+            isPassed[from][to] = true;
+            isPassed[to][from] = true;
+            canConnected(to, cnt + 1);
         }
     }
 }
