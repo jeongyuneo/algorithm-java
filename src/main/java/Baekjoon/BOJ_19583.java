3befbde103d8b7329ee3ed8249442f92e6358799
jeonyuneo <piq2255@gmail.com>
2023-06-13 16:35:18 +0900

    [UPDATE] 싸이버개강총회 - String 비교 풀이

--- src/main/java/Baekjoon/BOJ_19583.java
+++ src/main/java/Baekjoon/BOJ_19583.java
@@ -3,50 +3,40 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalTime;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 public class BOJ_19583 {
 
-    private static final String DELIMITER = ":";
-    private static final int HOUR = 0;
-    private static final int MINUTE = 1;
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        String[] start = stringTokenizer.nextToken().split(DELIMITER);
-        String[] end = stringTokenizer.nextToken().split(DELIMITER);
-        String[] endOfStreaming = stringTokenizer.nextToken().split(DELIMITER);
-        LocalTime startTime = LocalTime.of(Integer.parseInt(start[HOUR]), Integer.parseInt(start[MINUTE]));
-        LocalTime endTime = LocalTime.of(Integer.parseInt(end[HOUR]), Integer.parseInt(end[MINUTE]));
-        LocalTime endOfStreamingTime = LocalTime.of(Integer.parseInt(endOfStreaming[HOUR]), Integer.parseInt(endOfStreaming[MINUTE]));
-        Set<String> chatMembers = new HashSet<>();
-        Set<String> members = new HashSet<>();
-        while (true) {
-            String input = bufferedReader.readLine();
-            if (input == null) {
-                break;
-            }
+        String start = stringTokenizer.nextToken();
+        String end = stringTokenizer.nextToken();
+        String endOfStreaming = stringTokenizer.nextToken();
+        Set<String> attendingMembers = new HashSet<>();
+        Set<String> remainingMembers = new HashSet<>();
+        String input;
+        while ((input = bufferedReader.readLine()) != null) {
             stringTokenizer = new StringTokenizer(input);
-            String[] chat = stringTokenizer.nextToken().split(DELIMITER);
+            String chat = stringTokenizer.nextToken();
             String nickname = stringTokenizer.nextToken();
-            LocalTime chatTime = LocalTime.of(Integer.parseInt(chat[HOUR]), Integer.parseInt(chat[MINUTE]));
-            if (chatTime.isBefore(startTime.plusMinutes(1))) {
-                chatMembers.add(nickname);
+            if (isBefore(start, chat)) {
+                attendingMembers.add(nickname);
             }
-            if (chatTime.isAfter(endTime.minusMinutes(1)) && chatTime.isBefore(endOfStreamingTime.plusMinutes(1))) {
-                members.add(nickname);
-            }
-        }
-        int answer = 0;
-        for (String member : members) {
-            if (chatMembers.contains(member)) {
-                answer++;
+            if (isAfter(end, chat) && isBefore(endOfStreaming, chat) && attendingMembers.contains(nickname)) {
+                remainingMembers.add(nickname);
             }
         }
-        System.out.println(answer);
+        System.out.println(remainingMembers.size());
+    }
+
+    private static boolean isBefore(String start, String end) {
+        return start.compareTo(end) >= 0;
+    }
+
+    private static boolean isAfter(String start, String end) {
+        return start.compareTo(end) <= 0;
     }
 }
