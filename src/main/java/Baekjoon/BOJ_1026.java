e89f1c1ecf79c60a241605eed24dd68f0863e6c0
jeonyuneo <piq2255@gmail.com>
2023-07-24 23:32:58 +0900

    [UPDATE] 보물 - 재풀이

--- src/main/java/Baekjoon/BOJ_1026.java
+++ src/main/java/Baekjoon/BOJ_1026.java
@@ -4,26 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class BOJ_1026 {
+
+    private static final String DELIMITER = " ";
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
+        String[] inputA = bufferedReader.readLine().split(DELIMITER);
+        String[] inputB = bufferedReader.readLine().split(DELIMITER);
         int[] A = new int[n];
         int[] B = new int[n];
-        StringTokenizer inputA = new StringTokenizer(bufferedReader.readLine());
-        StringTokenizer inputB = new StringTokenizer(bufferedReader.readLine());
         for (int i = 0; i < n; i++) {
-            A[i] = Integer.parseInt(inputA.nextToken());
-            B[i] = Integer.parseInt(inputB.nextToken());
+            A[i] = Integer.parseInt(inputA[i]);
+            B[i] = Integer.parseInt(inputB[i]);
         }
         Arrays.sort(A);
         Arrays.sort(B);
         int S = 0;
         for (int i = 0; i < n; i++) {
-            S += A[i] * B[n - i - 1];
+            S += A[i] * B[n - 1 - i];
         }
         System.out.println(S);
     }
