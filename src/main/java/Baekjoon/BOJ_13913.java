2b4378b35764a4491e902ed22e1e634119fddafd
jeongyuneo <piq2255@gmail.com>
2023-08-08 09:30:04 +0900

    [UPDATE] 숨바꼭질 4 - 재풀이

--- src/main/java/Baekjoon/BOJ_13913.java
+++ src/main/java/Baekjoon/BOJ_13913.java
@@ -7,49 +7,72 @@ import java.util.*;
 
 public class BOJ_13913 {
 
-    private static final int NOT_VISIT = -1;
+    private static final int LIMIT = 100000;
+
+    private static int[] roots;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int k = Integer.parseInt(stringTokenizer.nextToken());
+        roots = new int[LIMIT + 1];
+        roots[n] = -1;
+        System.out.println(getResult(n, k));
+    }
 
-        Queue<Integer> subin = new ArrayDeque<>();
-        subin.offer(n);
-        int[] visited = new int[100001];
-        Arrays.fill(visited, -1);
-        visited[n] = Integer.MAX_VALUE;
-        int time = 0;
-        while (!subin.isEmpty()) {
-            int size = subin.size();
-            while (size-- > 0) {
-                int current = subin.poll();
-                if (current == k) {
-                    Stack traces = new Stack();
-                    int parent = k;
-                    while (parent != Integer.MAX_VALUE) {
-                        traces.push(parent);
-                        parent = visited[parent];
-                    }
-                    StringBuilder answer = new StringBuilder();
-                    answer.append(time).append("\n");
-                    while (!traces.isEmpty()) {
-                        answer.append(traces.pop()).append(" ");
-                    }
-                    System.out.println(answer);
-                    break;
-                }
-
-                int[] nexts = {current - 1, current + 1, current * 2};
-                for (int next : nexts) {
-                    if (next >= 0 && next <= 100000 && visited[next] == NOT_VISIT) {
-                        visited[next] = current;
-                        subin.offer(next);
-                    }
-                }
-            }
-            time++;
+    private static StringBuilder getResult(int n, int k) {
+        StringBuilder result = new StringBuilder();
+        result.append(getMinTime(n, k)).append("\n");
+        Stack<Integer> wayToFindSister = getWayToFindSister(k);
+        while (!wayToFindSister.isEmpty()) {
+            result.append(wayToFindSister.pop()).append(" ");
         }
+        return result;
+    }
+
+    private static Stack<Integer> getWayToFindSister(int k) {
+        Stack<Integer> wayToFindSister = new Stack<>();
+        wayToFindSister.push(k);
+        int root = k;
+        while (roots[root] != -1) {
+            wayToFindSister.push(roots[root]);
+            root = roots[root];
+        }
+        return wayToFindSister;
+    }
+
+    private static int getMinTime(int n, int k) {
+        boolean[] isVisited = new boolean[LIMIT + 1];
+        int[] times = new int[LIMIT + 1];
+        Arrays.fill(times, Integer.MAX_VALUE);
+        times[n] = 0;
+        Queue<int[]> subin = new ArrayDeque<>();
+        subin.offer(new int[]{n, 0});
+        while (!subin.isEmpty()) {
+            int[] current = subin.poll();
+            int location = current[0];
+            int time = current[1];
+            if (location == k) {
+                return time;
+            }
+            isVisited[location] = true;
+            if (location - 1 >= 0 && !isVisited[location - 1] && times[location - 1] > time + 1) {
+                roots[location - 1] = location;
+                times[location - 1] = time + 1;
+                subin.offer(new int[]{location - 1, time + 1});
+            }
+            if (location + 1 <= LIMIT && !isVisited[location + 1] && times[location + 1] > time + 1) {
+                roots[location + 1] = location;
+                times[location + 1] = time + 1;
+                subin.offer(new int[]{location + 1, time + 1});
+            }
+            if (location * 2 <= LIMIT && !isVisited[location * 2] && times[location * 2] > time + 1) {
+                roots[location * 2] = location;
+                times[location * 2] = time + 1;
+                subin.offer(new int[]{location * 2, time + 1});
+            }
+        }
+        return 0;
     }
 }
