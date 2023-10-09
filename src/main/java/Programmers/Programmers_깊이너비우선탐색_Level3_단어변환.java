8e30ad2e276cc450cc407b927166347655a8b2b7
jeongyuneo <piq2255@gmail.com>
2023-10-09 23:49:30 +0900

    [UPDATE] 단어 변환 - 리팩토링

--- src/main/java/Programmers/Programmers_깊이너비우선탐색_Level3_단어변환.java
+++ src/main/java/Programmers/Programmers_깊이너비우선탐색_Level3_단어변환.java
@@ -1,8 +1,5 @@
 package Programmers;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Programmers_깊이너비우선탐색_Level3_단어변환 {
 
     public static void main(String[] args) {
@@ -10,43 +7,40 @@ public class Programmers_깊이너비우선탐색_Level3_단어변환 {
         System.out.println(solution("hit", "cog", new String[]{"hot", "dot", "dog", "lot", "log"}));
     }
 
-    private static List<String> wordList;
     private static boolean[] isChecked;
-    private static int length;
     private static int min;
 
     public static int solution(String begin, String target, String[] words) {
-        length = begin.length();
-        wordList = Arrays.asList(words);
-        isChecked = new boolean[words.length + 1];
+        isChecked = new boolean[words.length];
         min = Integer.MAX_VALUE;
-        change(begin, target, 0);
+        change(words, begin, target, 0);
         if (min == Integer.MAX_VALUE) {
             return 0;
         }
         return min;
     }
 
-    private static void change(String word, String target, int cnt) {
+    private static void change(String[] words, String word, String target, int cnt) {
         if (word.equals(target)) {
             min = Math.min(min, cnt);
             return;
         }
-        for (int j = 0, wordListSize = wordList.size(); j < wordListSize; j++) {
-            if (isChecked[j + 1]) {
-                continue;
-            }
-            int diffrent = 0;
-            for (int i = 0; i < length; i++) {
-                if (word.charAt(i) != wordList.get(j).charAt(i)) {
-                    diffrent++;
-                }
-            }
-            if (diffrent == 1) {
-                isChecked[j + 1] = true;
-                change(wordList.get(j), target, cnt + 1);
-                isChecked[j + 1] = false;
+        for (int i = 0, wordsSize = words.length; i < wordsSize; i++) {
+            if (!isChecked[i] && getDifferentCount(word, words[i]) == 1) {
+                isChecked[i] = true;
+                change(words, words[i], target, cnt + 1);
+                isChecked[i] = false;
             }
         }
+    }
+
+    private static int getDifferentCount(String word, String candidate) {
+        int diffrent = 0;
+        for (int j = 0; j < word.length(); j++) {
+            if (word.charAt(j) != candidate.charAt(j)) {
+                diffrent++;
+            }
+        }
+        return diffrent;
     }
 }
