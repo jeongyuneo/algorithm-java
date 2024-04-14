9154cd77a4dbcc40b8483ec12229fd89c01012d6
jeongyuneo <piq2255@gmail.com>
2024-04-14 22:26:41 +0900

    [UPDATE] Add Two Numbers - 개선

--- src/main/java/LeetCode/LeetCode_2.java
+++ src/main/java/LeetCode/LeetCode_2.java
@@ -21,51 +21,22 @@ public class LeetCode_2 {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        String result = "";
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val;
-            if (l1.next != null) {
-                l1.next.val += sum / 10;
-            } else if (l2.next != null) {
-                l2.next.val += sum / 10;
-            } else if (sum / 10 > 0) {
-                l1.next = new ListNode(sum / 10);
+        ListNode tail = new ListNode();
+        ListNode result = tail;
+        int sum = 0;
+        while (l1 != null || l2 != null || sum != 0) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            result += sum % 10;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        while (l1 != null) {
-            result += l1.val % 10;
-            if (l1.next != null) {
-                l1.next.val += l1.val / 10;
-            } else if (l1.val / 10 > 0) {
-                result += l1.val / 10;
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
-            l1 = l1.next;
+            tail.next = new ListNode(sum % 10);
+            tail = tail.next;
+            sum /= 10;
         }
-        while (l2 != null) {
-            result += l2.val % 10;
-            if (l2.next != null) {
-                l2.next.val += l2.val / 10;
-            } else if (l2.val / 10 > 0) {
-                result += l2.val / 10;
-            }
-            l2 = l2.next;
-        }
-        return toListNode(result);
-    }
-
-    private ListNode toListNode(String num) {
-        ListNode previous = new ListNode(toInteger(num.charAt(num.length() - 1)));
-        for (int idx = num.length() - 2; idx >= 0; idx--) {
-            ListNode current = new ListNode(toInteger(num.charAt(idx)), previous);
-            previous = current;
-        }
-        return previous;
-    }
-
-    private int toInteger(char character) {
-        return character - '0';
+        return result.next;
     }
 }
