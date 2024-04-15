1a983d262dfe4500a58b6b564d80f6e9de9399d9
jeongyuneo <piq2255@gmail.com>
2024-04-16 00:22:38 +0900

    [UPDATE] Integer to Roman - 개선

--- src/main/java/LeetCode/LeetCode_12.java
+++ src/main/java/LeetCode/LeetCode_12.java
@@ -1,30 +1,14 @@
 package LeetCode;
 
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
-
 public class LeetCode_12 {
 
     public String intToRoman(int num) {
-        Map<Integer, String> romans = new TreeMap<>(Comparator.reverseOrder());
-        romans.put(1000, "M");
-        romans.put(900, "CM");
-        romans.put(500, "D");
-        romans.put(400, "CD");
-        romans.put(100, "C");
-        romans.put(90, "XC");
-        romans.put(50, "L");
-        romans.put(40, "XL");
-        romans.put(10, "X");
-        romans.put(9, "IX");
-        romans.put(5, "V");
-        romans.put(4, "IV");
-        romans.put(1, "I");
+        int[] integers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         String answer = "";
-        for (Map.Entry<Integer, String> entry : romans.entrySet()) {
-            answer += entry.getValue().repeat(num / entry.getKey());
-            num %= entry.getKey();
+        for (int idx = 0, len = integers.length; idx < len; idx++) {
+            answer += romans[idx].repeat(num / integers[idx]);
+            num %= integers[idx];
         }
         return answer;
     }
